de222decc48e94ca4a982a5068eb95b4bd35d8a7
SAMAEV <andreysamaev@gmail.com>
2022-07-09 15:35:44 +0300

    checkPressed

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,38 @@
-import React from 'react';
+import { Component, ReactNode } from 'react';
 
-export const App: React.FC = () => (
-  <div className="App">
-    <p>Nothing was pressed yet</p>
-    <p>The last pressed key is [Enter]</p>
-  </div>
-);
+type State = {
+  msg: string,
+};
+
+export class App extends Component<{}, State> {
+  state = {
+    msg: 'Nothing pressed yet',
+  };
+
+  componentDidMount() {
+    document.addEventListener('keyup', (event: KeyboardEvent) => {
+      if (event.key) {
+        this.setState({ msg: `The last pressed key is ${event.key}` });
+      }
+    });
+  }
+
+  componentWillUnmount() {
+    document.addEventListener('keyup', (event: KeyboardEvent) => {
+      if (event.key) {
+        this.setState({ msg: `The last pressed key is ${event.key}` });
+      }
+    });
+  }
+
+  render(): ReactNode {
+    const { msg } = this.state;
+
+    return (
+      <div>
+        <h1>Checking...</h1>
+        <h2>{msg}</h2>
+      </div>
+    );
+  }
+}
