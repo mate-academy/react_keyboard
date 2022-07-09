1dc63e07631ff69fc822c6cc0d46e8b4077c7fcc
Andrii Nychaiuk <anichajuk@gmail.com>
2022-07-10 02:13:16 +0300

    solution

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,34 @@
 import React from 'react';
+import { Keyboard } from './keyboard';
 
-export const App: React.FC = () => (
-  <div className="App">
-    <p>Nothing was pressed yet</p>
-    <p>The last pressed key is [Enter]</p>
-  </div>
-);
+interface State{
+  isVisible: boolean
+}
+
+export class App extends React.Component<{}, State> {
+  state = {
+    isVisible: true,
+  };
+
+  switcher = () => {
+    return this.state.isVisible
+      ? this.setState({ isVisible: false })
+      : this.setState({ isVisible: true });
+  };
+
+  render() {
+    return (
+      <>
+        {this.state.isVisible && (
+          <Keyboard />
+        )}
+        <button
+          type="button"
+          onClick={this.switcher}
+        >
+          Switch
+        </button>
+      </>
+    );
+  }
+}
---
+++ src/keyboard.tsx
@@ -0,0 +1,34 @@
+import React from 'react';
+
+interface State{
+  phrase: string;
+}
+
+export class Keyboard extends React.Component<{}, State> {
+  state = {
+    phrase: 'Nothing was pressed yet',
+  };
+
+  componentDidMount() {
+    document.addEventListener('keyup', this.callback);
+  }
+
+  componentWillUnmount() {
+    document.removeEventListener('keyup', this.callback);
+  }
+
+  callback = (event: KeyboardEvent) => {
+    // eslint-disable-next-line no-console
+    console.log(event.key);
+
+    this.setState({ phrase: `The last pressed key is ${event.key}` });
+  };
+
+  render() {
+    return (
+      <div className="App">
+        <p>{this.state.phrase}</p>
+      </div>
+    );
+  }
+}
