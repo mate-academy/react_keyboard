a323e55da750f46afd25b668a724ceca2636b5c5
SpaceHook <maxdumiak39@gmail.com>
2022-07-09 22:36:13 +0300

    react keboard ready

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,35 @@
 import React from 'react';
 
-export const App: React.FC = () => (
-  <div className="App">
-    <p>Nothing was pressed yet</p>
-    <p>The last pressed key is [Enter]</p>
-  </div>
-);
+type State = {
+  pressedKey: string,
+};
+
+export class App extends React.Component<{}, State> {
+  state = {
+    pressedKey: 'Nothing was pressed yet',
+  };
+
+  componentDidMount() {
+    document.addEventListener('keyup', this.listener);
+  }
+
+  componentWillUnmount() {
+    document.removeEventListener('keyup', this.listener);
+  }
+
+  listener = (event: KeyboardEvent) => {
+    if (event.key === ' ') {
+      this.setState({ pressedKey: 'The last pressed key is Space' });
+    } else {
+      this.setState({ pressedKey: `The last pressed key is ${event.key}` });
+    }
+  };
+
+  render() {
+    return (
+      <div className="App">
+        <p>{this.state.pressedKey}</p>
+      </div>
+    );
+  }
+}
