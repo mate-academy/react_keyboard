4826e3c596f947ed8e6928acce65ed7336c9e2ae
AndrewKhorsun <andrewkhorsun@gmail.com>
2022-07-10 17:34:42 +0300

    keyboard

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,47 @@
 import React from 'react';
 
-export const App: React.FC = () => (
-  <div className="App">
-    <p>Nothing was pressed yet</p>
-    <p>The last pressed key is [Enter]</p>
-  </div>
-);
+// export const App: React.FC = () => (
+//   <div className="App">
+//     <p>Nothing was pressed yet</p>
+//     <p>The last pressed key is [Enter]</p>
+//   </div>
+// );
+
+
+type State = {
+  pressedKey: string,
+};
+
+export class App extends React.Component<{}, State> {
+  state = {
+    pressedKey: 'Nothing was pressed yet',
+  };
+
+  componentDidMount() {
+    document.addEventListener('keyup', (event: KeyboardEvent) => {
+      if (event.key === ' ') {
+        this.setState({ pressedKey: 'The last pressed key is space' });
+      } else {
+        this.setState({ pressedKey: `The last pressed key is ${event.key}` });
+      }
+    });
+  }
+
+  componentWillUnmount() {
+    document.addEventListener('keyup', (event: KeyboardEvent) => {
+      if (event.key === ' ') {
+        this.setState({ pressedKey: 'The last pressed key is space' });
+      } else {
+        this.setState({ pressedKey: `The last pressed key is ${event.key}` });
+      }
+    });
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <p>{this.state.pressedKey}</p>
+      </div>
+    );
+  }
+}
