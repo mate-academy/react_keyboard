31c291a6bbaf291dbf044c77ae0c9d9e61483334
Kate Khon <katerynakhon@gmail.com>
2022-07-10 12:55:47 +0300

    <p> was added

--- src/App.tsx
+++ src/App.tsx
@@ -26,9 +26,11 @@ export class App extends React.Component<{}, State> {
   render() {
     return (
       <div className="App">
-        {this.state.pressedKey
-          ? `The last pressed key is [${this.state.pressedKey}]`
-          : 'Nothing was pressed yet'}
+        <p>
+          {this.state.pressedKey
+            ? `The last pressed key is [${this.state.pressedKey}]`
+            : 'Nothing was pressed yet'}
+        </p>
       </div>
     );
   }
