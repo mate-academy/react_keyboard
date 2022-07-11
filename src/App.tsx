2e28e0b1a8d949170d081dd72bbda0950ce273c5
Andrew Bilous <bilous.a.work@gmail.com>
2022-07-11 13:03:27 +0300

    some comments deleted

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,5 @@
 import React from 'react';
 
-// export const App: React.FC = () => (
-//   <div className="App">
-//     <p>Nothing was pressed yet</p>
-//     <p>The last pressed key is [Enter]</p>
-//   </div>
-// );
-
 type State = {
   pressedKey: string,
 };
