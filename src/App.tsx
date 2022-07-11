a9137f7e226c99cd642c40107903907267e092de
viktorlyt <viktorlyt@gmail.com>
2022-07-11 15:37:05 +0300

    solution

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,45 @@
-import React from 'react';
+// import { event } from 'cypress/types/jquery';
+import { Component } from 'react';
 
-export const App: React.FC = () => (
-  <div className="App">
-    <p>Nothing was pressed yet</p>
-    <p>The last pressed key is [Enter]</p>
-  </div>
-);
+type State = {
+  pressedKey: string,
+};
+
+export class App extends Component<{}, State> {
+  state: Readonly<State> = {
+    pressedKey: 'Nothing was pressed yet',
+  };
+
+  componentDidMount() {
+    document.addEventListener('keyup', this.handleKeyboardEvent);
+  }
+
+  componentWillUnmount() {
+    document.removeEventListener('keyup', this.handleKeyboardEvent);
+  }
+
+  handleKeyboardEvent = (event: KeyboardEvent) => {
+    // eslint-disable-next-line no-console
+    console.log(event.key);
+
+    this.setState({ pressedKey: event.key });
+  };
+
+  render() {
+    const { pressedKey } = this.state;
+
+    return (
+      <div className="App">
+        {pressedKey === 'Nothing was pressed yet'
+          ? <p>{pressedKey}</p>
+          : (
+            <p>
+              The last pressed key is
+              {' '}
+              {`"${pressedKey}"`}
+            </p>
+          )}
+      </div>
+    );
+  }
+}
