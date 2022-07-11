4891aaca3cb5c35bf7de630343b52abb74afc498
Lesya Savka <lesyasimple@gmail.com>
2022-07-11 20:56:29 +0300

    Ver_01

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,46 @@
-import React from 'react';
+import { Component } from 'react';
 
-export const App: React.FC = () => (
-  <div className="App">
-    <p>Nothing was pressed yet</p>
-    <p>The last pressed key is [Enter]</p>
-  </div>
-);
+type State = {
+  pressedKey: boolean,
+  message: string,
+};
+
+export class App extends Component<{}, State> {
+  state = {
+    pressedKey: true,
+    message: 'Nothing was pressed yet',
+  };
+
+  componentDidMount() {
+    document.addEventListener('keyup', this.handleClick);
+  }
+
+  componentWillUnmount() {
+    document.removeEventListener('keyup', this.handleClick);
+  }
+
+  handleClick = (event: KeyboardEvent) => {
+    if (event.key) {
+      this.setState({ pressedKey: true, message: `The last pressed key is ${event.key}` });
+    }
+
+    if (event.key === 'Escape') {
+      this.setState({ pressedKey: false });
+    }
+
+    if (event.key === ' ') {
+      this.setState({
+        pressedKey: true,
+        message: 'Invalid key, please choose the letter',
+      });
+    }
+  };
+
+  render(): React.ReactNode {
+    return (
+      <div className="App">
+        {this.state.pressedKey && <p>{this.state.message}</p>}
+      </div>
+    );
+  }
+}
