fbaff09c257172511a1bde6844b13ee16e1e52af
Ivan Grabovsky <ivan.grabovsky.ua@gmail.com>
2022-07-12 13:43:32 +0300

    add value for pressing Space

--- src/App.tsx
+++ src/App.tsx
@@ -20,7 +20,7 @@ export class App extends Component<{}, State> {
   handleDocumentKeyUp = (event: KeyboardEvent) => {
     if (event.key) {
       this.setState({
-        notification: `The last pressed key is [${event.key}]`,
+        notification: `The last pressed key is [${event.key === ' ' ? 'Space' : event.key}]`,
       });
     }
   };
