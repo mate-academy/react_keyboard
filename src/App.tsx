34b85540b52ff46cf0f53cc317eebbf04147eedb
Oleksii Zasiedka <alex.zasedka@gmail.com>
2022-08-03 12:20:29 +0300

    Fixed space issue

--- src/App.tsx
+++ src/App.tsx
@@ -18,8 +18,12 @@ export class App extends React.Component<{}, State> {
   }
 
   keyListener = (event: KeyboardEvent) => {
+    // eslint-disable-next-line no-console
+    console.log(event.code === 'Space');
     this.setState({
-      pressedKey: event.key,
+      pressedKey: event.code === 'Space'
+        ? 'Space'
+        : event.key,
     });
   };
 
@@ -28,11 +32,7 @@ export class App extends React.Component<{}, State> {
       <div className="App">
         <p className="App__message">
           { this.state.pressedKey
-            ? `The last pressed key is [${
-              this.state.pressedKey === ' '
-                ? 'Space'
-                : this.state.pressedKey
-            }]`
+            ? `The last pressed key is [${this.state.pressedKey}]`
             : 'Nothing was pressed yet' }
         </p>
       </div>
