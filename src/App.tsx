5809be54416b1a64252d7e7060bfbcdb24e56622
Oleksii Zasiedka <alex.zasedka@gmail.com>
2022-08-03 12:12:49 +0300

    Fixed space issue

--- src/App.tsx
+++ src/App.tsx
@@ -28,7 +28,11 @@ export class App extends React.Component<{}, State> {
       <div className="App">
         <p className="App__message">
           { this.state.pressedKey
-            ? `The last pressed key is [${this.state.pressedKey}]`
+            ? `The last pressed key is [${
+              this.state.pressedKey === ' '
+                ? 'Space'
+                : this.state.pressedKey
+            }]`
             : 'Nothing was pressed yet' }
         </p>
       </div>
