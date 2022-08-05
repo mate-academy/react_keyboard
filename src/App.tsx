88917eda94dfbebd8e40fa91ff87346f9881be15
Liza Zavalko <asklepia2012@gmail.com>
2022-08-05 15:57:37 +0300

    Added space key

--- src/App.tsx
+++ src/App.tsx
@@ -22,6 +22,10 @@ export class App extends React.Component< {}, State > {
   );
 
   render() {
+    if (this.state.pressedKey === ' ') {
+      this.setState({ pressedKey: 'Space' });
+    }
+
     return (
       <div className="App">
         <p className="App__message">
