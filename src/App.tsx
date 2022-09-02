141928bd9272b698b9277cc8faffb5e99d809e3d
Natalya Tsybulko <tsybulko.natalya395@gmail.com>
2022-09-03 02:51:07 +0300

    solve the reactKeyboard problem

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,38 @@
 import React from 'react';
 
-export const App: React.FC = () => (
-  <div className="App">
-    <p className="App__message">The last pressed key is [Enter]</p>
-  </div>
-);
+type State = {
+  pressedKey: string;
+};
+export class App extends React.Component<{}, State> {
+  state = {
+    pressedKey: 'Nothing was pressed yet',
+  };
+
+  componentDidMount() {
+    document.addEventListener('keyup', this.handleDocumentClick);
+  }
+
+  componentWillUnmount() {
+    document.removeEventListener('keyup', this.handleDocumentClick);
+  }
+
+  handleDocumentClick = (event: KeyboardEvent) => {
+    this.setState(
+      {
+        pressedKey: `The last pressed key is [${event.key}]`,
+      },
+    );
+  };
+
+  render() {
+    const { pressedKey } = this.state;
+
+    return (
+      <div className="App">
+        <p className="App__message">
+          {pressedKey}
+        </p>
+      </div>
+    );
+  }
+}
