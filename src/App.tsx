0a7b3275c1d96990c086052437156be705dacb24
Andrii Dmytruk <andriydmitr2014@gmail.com>
2022-09-07 16:16:02 +0300

    react keyboard implemented

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,41 @@
+/* eslint-disable no-restricted-globals */
+/* eslint-disable react/prefer-stateless-function */
 import React from 'react';
 
-export const App: React.FC = () => (
-  <div className="App">
-    <p className="App__message">The last pressed key is [Enter]</p>
-  </div>
-);
+type State = {
+  pressedKey: string;
+};
+
+export class App extends React.Component<{}, State> {
+  state: Readonly<State> = {
+    pressedKey: '',
+  };
+
+  componentDidMount() {
+    document.addEventListener('keyup', this.handleKeyboardEvent);
+  }
+
+  componentWillUnmount() {
+    document.removeEventListener('keyup', this.handleKeyboardEvent);
+  }
+
+  handleKeyboardEvent = (event: KeyboardEvent) => {
+    this.setState({ pressedKey: event.key });
+  };
+
+  render() {
+    const { pressedKey } = this.state;
+
+    return (
+      <div className="App">
+        <p className="App__message">
+          {
+            pressedKey.length
+              ? `The last pressed key is [${pressedKey}]`
+              : 'Nothing was pressed yet'
+          }
+        </p>
+      </div>
+    );
+  }
+}
