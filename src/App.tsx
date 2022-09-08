9f18a7f679075d5b9a89343539041a0283bf9d76
Andrii Stiebalov <stiebalov@gmail.com>
2022-09-08 14:53:29 +0300

    key event listener was added

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,33 @@
 import React from 'react';
 
-export const App: React.FC = () => (
-  <div className="App">
-    <p className="App__message">The last pressed key is [Enter]</p>
-  </div>
-);
+type State = {
+  lastKey: string;
+};
+
+export class App extends React.Component<{}, State> {
+  state: Readonly<State> = {
+    lastKey: 'Nothing was pressed yet',
+  };
+
+  componentDidMount() {
+    document.addEventListener('keyup', this.switchKey);
+  }
+
+  componentWillUnmount() {
+    document.removeEventListener('keyup', this.switchKey);
+  }
+
+  switchKey = (event: KeyboardEvent) => {
+    this.setState({ lastKey: `The last pressed key is [${event.key}]` });
+  };
+
+  render(): React.ReactNode {
+    const { lastKey } = this.state;
+
+    return (
+      <div className="App">
+        <p className="App__message">{lastKey}</p>
+      </div>
+    );
+  }
+}
