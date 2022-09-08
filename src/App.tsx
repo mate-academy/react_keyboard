91e825950087c7fff8cd11ee0fba871102d31d1a
Jamal Amoian <leonidagutin09@gmail.com>
2022-09-08 14:25:10 +0300

    Solution

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,51 @@
 import React from 'react';
 
+type State = {
+  pressedKey: boolean,
+  userPressed: string,
+};
+
+export class Click extends React.Component<{}, State> {
+  state: Readonly<State> = {
+    pressedKey: false,
+    userPressed: '',
+  };
+
+  componentDidMount() {
+    document.addEventListener('keyup', this.handleDocumentClick);
+  }
+
+  componentWillUnmount() {
+    document.removeEventListener('keyup', this.handleDocumentClick);
+  }
+
+  handleDocumentClick = (event: KeyboardEvent) => {
+    if (event.key) {
+      const userPressed1 = event.key;
+
+      this.setState({ userPressed: userPressed1 });
+      this.setState({ pressedKey: true });
+    }
+  };
+
+  render() {
+    const { pressedKey, userPressed } = this.state;
+
+    return (
+      <>
+        {
+          !pressedKey
+            ? <p>Nothing was pressed yet</p>
+            : <p>{`The last pressed key is [${userPressed}]`}</p>
+        }
+      </>
+    );
+  }
+}
+
 export const App: React.FC = () => (
+
   <div className="App">
-    <p className="App__message">The last pressed key is [Enter]</p>
+    <Click />
   </div>
 );
