f2b58068ee1bbc246e38c50a788afa3cc2a69c17
Oleh Mykolaichuk <ooolehmy@gmail.com>
2022-09-08 17:45:55 +0300

    Solution implemented

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,66 @@
-import React from 'react';
+import { Component } from 'react';
+import classNames from 'classnames';
 
-export const App: React.FC = () => (
-  <div className="App">
-    <p className="App__message">The last pressed key is [Enter]</p>
-  </div>
-);
+type State = {
+  key: string;
+  isKeyDown: boolean;
+};
+
+export class App extends Component<{}, State> {
+  state: Readonly<State> = {
+    key: '',
+    isKeyDown: false,
+  };
+
+  componentDidMount() {
+    document.addEventListener('keyup', this.handleKeyUp);
+  }
+
+  componentWillUnmount() {
+    document.removeEventListener('keyup', this.handleKeyUp);
+  }
+
+  handleKeyUp = (event: KeyboardEvent) => {
+    const eventKey = event.key === ' '
+      ? 'backspace'
+      : event.key;
+
+    this.setState({ key: eventKey, isKeyDown: false });
+  };
+
+  handleKeyDown = (event: KeyboardEvent) => {
+    if (event) {
+      this.setState({ isKeyDown: true });
+    }
+  };
+
+  render() {
+    const { key, isKeyDown } = this.state;
+
+    document.addEventListener('keydown', this.handleKeyDown);
+
+    return (
+      <div className="App">
+        <p className="App__message">
+          {key ? (
+            <>
+              The last pressed key is
+              <span
+                className={classNames(
+                  'App__message-key',
+                  {
+                    'App__message-key--keydown': isKeyDown,
+                  },
+                )}
+              >
+                {key}
+              </span>
+            </>
+          ) : (
+            'Nothing was pressed yet'
+          )}
+        </p>
+      </div>
+    );
+  }
+}
