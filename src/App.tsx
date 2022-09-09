d8f9a42f32027cd59fa8e8e2b6e913934451d93e
Oleksii Pustovit <91pustovit91@gmail.com>
2022-09-09 15:57:52 +0300

    Solution of react_keyboard

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,51 @@
 import React from 'react';
 
+type State = {
+  pressedKey: boolean;
+  pressedUserKey: string;
+};
+
+export class Click extends React.Component<{}, State> {
+  state: Readonly<State> = {
+    pressedKey: false,
+    pressedUserKey: '',
+  };
+
+  componentDidMount() {
+    document.addEventListener('keyup', this.handleKeyUp);
+  }
+
+  componentWillUnmount() {
+    document.removeEventListener('keyup', this.handleKeyUp);
+  }
+
+  handleKeyUp = (event: KeyboardEvent) => {
+    if (event.key) {
+      this.setState({
+        pressedKey: true,
+        pressedUserKey: event.key,
+      });
+    }
+  };
+
+  render() {
+    return (
+      <div>
+        <h1>Press any key</h1>
+        <p className="App__message">
+          {
+            this.state.pressedKey
+              ? `The last pressed key is [${this.state.pressedUserKey}]`
+              : 'Nothing was pressed yet'
+          }
+        </p>
+      </div>
+    );
+  }
+}
+
 export const App: React.FC = () => (
   <div className="App">
-    <p className="App__message">The last pressed key is [Enter]</p>
+    <Click />
   </div>
 );
