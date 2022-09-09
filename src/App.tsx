aac845f50f228fb2fbc8f67b6146f4da4815a5f2
Kostya Kosyuk <80011474+Kostya7337@users.noreply.github.com>
2022-09-09 14:48:50 +0300

    solution 1: fixed codeStyle

--- src/App.tsx
+++ src/App.tsx
@@ -25,11 +25,9 @@ export class App extends Component<{}, State> {
     return (
       <div className="App">
         <p className="App__message">
-          {
-            this.state.pressedKey !== ''
-              ? `The last pressed key is [${this.state.pressedKey}]`
-              : 'Nothing was pressed yet'
-          }
+          {this.state.pressedKey !== ''
+            ? `The last pressed key is [${this.state.pressedKey}]`
+            : 'Nothing was pressed yet'}
         </p>
       </div>
     );
