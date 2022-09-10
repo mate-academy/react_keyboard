af6cf3c27b0bc452cfb3caf38f57e0594fe07b3f
Igor Bondarenko <igorbondarenko35@gmail.com>
2022-09-10 23:06:44 +0300

    keyloger was created

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,41 @@
 import React from 'react';
 
-export const App: React.FC = () => (
-  <div className="App">
-    <p className="App__message">The last pressed key is [Enter]</p>
-  </div>
-);
+type State = {
+  lastPressedKey: string;
+};
+
+export class App extends React.Component<{}, State> {
+  state = {
+    lastPressedKey: '',
+  };
+
+  componentDidMount(): void {
+    document.addEventListener('keyup', (this.pressedKey));
+  }
+
+  componentWillUnmount() {
+    document.removeEventListener('keyup', (this.pressedKey));
+  }
+
+  pressedKey = (event: KeyboardEvent): void => {
+    this.setState({
+      lastPressedKey: event.key,
+    });
+  };
+
+  render() {
+    const { lastPressedKey } = this.state;
+
+    return (
+      <div className="App">
+        <p className="App__message">
+          {
+            lastPressedKey
+              ? `The last pressed key is [${lastPressedKey}]`
+              : 'Nothing was pressed yet'
+          }
+        </p>
+      </div>
+    );
+  }
+}
