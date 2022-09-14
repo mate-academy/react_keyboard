d84d59a2c320d1b768adac4a452fdbc8c3edd056
Kolia Plemennyk <plemianniknikolay@gmail.com>
2022-09-14 17:53:41 +0300

    rework add separate method for setState

--- src/App.tsx
+++ src/App.tsx
@@ -10,16 +10,16 @@ export class App extends Component<{}, State> {
   };
 
   componentDidMount() {
-    document.addEventListener('keyup', (event: KeyboardEvent) => {
-      this.setState({ pressedKey: `The last pressed key is [${event.key}]` });
-    });
+    document.addEventListener('keyup', this.separateMethod);
   }
 
   componentWillUnmount() {
-    document.removeEventListener('keyup', (event: KeyboardEvent) => {
-      this.setState({ pressedKey: `The last pressed key is [${event.key}]` });
-    });
+    document.removeEventListener('keyup', this.separateMethod);
   }
+
+  separateMethod = (event: KeyboardEvent) => {
+    this.setState({ pressedKey: `The last pressed key is [${event.key}]` });
+  };
 
   render() {
     return (
