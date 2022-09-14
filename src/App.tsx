62d791fb02067f975aba63fcdb076babbbde88fe
Kolia Plemennyk <plemianniknikolay@gmail.com>
2022-09-14 18:28:28 +0300

    rework (naming fix2 and cute cat) - pressedKey -> pressedKeyOrMaybeMessageWhoKnows

--- src/App.tsx
+++ src/App.tsx
@@ -1,37 +1,72 @@
 import { Component } from 'react';
 
 type State = {
-  pressedKey: string,
+  pressedKeyOrMaybeMessageWhoKnows: string,
 };
 
 export class App extends Component<{}, State> {
   state = {
-    pressedKey: 'Nothing was pressed yet',
+    pressedKeyOrMaybeMessageWhoKnows: 'Nothing was pressed yet',
   };
 
   componentDidMount() {
     document.addEventListener(
       'keyup',
-      this.changerOfPressedKeyValueInStateObject,
+      this.changerOfpressedKeyOrMaybeMessageWhoKnowsValueInStateObject,
     );
   }
 
   componentWillUnmount() {
     document.removeEventListener(
       'keyup',
-      this.changerOfPressedKeyValueInStateObject,
+      this.changerOfpressedKeyOrMaybeMessageWhoKnowsValueInStateObject,
     );
   }
 
-  changerOfPressedKeyValueInStateObject = (event: KeyboardEvent) => {
-    this.setState({ pressedKey: `The last pressed key is [${event.key}]` });
+  changerOfpressedKeyOrMaybeMessageWhoKnowsValueInStateObject = (
+    event: KeyboardEvent,
+  ) => {
+    this.setState({ pressedKeyOrMaybeMessageWhoKnows: `The last pressed key is [${event.key}]` });
   };
 
   render() {
     return (
       <div className="App">
-        <p className="App__message">{this.state.pressedKey}</p>
+        <p className="App__message">
+          {this.state.pressedKeyOrMaybeMessageWhoKnows}
+        </p>
       </div>
     );
   }
 }
+
+/* and cute cat
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢀⣀⠀⠀⠀⠀⠀⠀⠀⡴⠖⢆⢀⡀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣾⠛⠛⢹⡇⠀⠀⠀⠀⠀⠀⡇⠀⠈⠉⢹⡆⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠈⠳⠤⠟⠀⠀⠀⠀⠀⠀⠀⠻⠤⠤⠔⠋⠀⠀⠀⠀⣠⢤⠀⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢰⠃⠈⢧⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢀⡤⢤⡀⠀⠀⠀⠀⢰⠛⠲⢤⡀⠀⠀⠀⠀⠀⠀⠀⢀⡟⠀⠀⠈⣇⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⣠⡤⠼⠀⠀⢹⡀⠀⠀⠀⢸⡀⠀⠀⠙⠦⣀⢀⡠⠤⠤⠖⠚⠁⣠⡤⣄⣸⡄⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⢻⡀⠀⠀⠀⢰⠇⠀⠀⠀⠀⢧⠀⠀⠀⠀⠈⠣⣤⠖⠒⠀⠀⠀⠀⠀⣴⣮⠹⢦⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠉⠓⠒⠒⠋⠀⠀⠀⠀⠀⠸⡆⠀⠀⠀⠀⠀⢀⣠⣤⡀⠀⠀⠀⠀⢹⣿⠀⣨⣷⡂⡒⠂
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⡇⠀⠀⠀⠀⠀⢹⡹⢿⣿⡇⠀⠀⣴⣿⡄⠘⠛⠹⡧⢌⡉
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⡇⠀⠀⠀⠀⠀⠀⠉⣉⢭⣶⡆⠀⠉⢹⡁⢀⠀⢠⠇⠀⠈
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢳⡄⠀⠀⠀⠀⠠⠊⡠⠃⡜⠐⠒⠤⠚⠉⠁⢀⡞⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠉⠓⠤⣀⠀⠀⠰⠁⢠⡇⠀⠀⠀⠀⠀⢀⡞⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⡖⠀⠉⠁⠀⠀⠀⠀⠀⠀⠀⠀⠀⢸⠀⠀⠀⠀⠀
+⠀⠀⣀⡀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣠⠼⠁⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠘⣆⠀⠀⠀⠀
+⢸⡏⠉⠉⠙⢦⡀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣠⠴⠋⠁⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠘⡆⠀⠀⠀
+⠈⠓⠦⡄⠀⠀⣷⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⡴⠋⠁⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣧⠀⠀⠀
+⠀⠀⢀⡇⠀⢠⠇⠀⠀⠀⠀⠀⠀⠀⠀⢀⡞⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⡇⠀⠀⠀
+⠀⢀⠞⠀⢠⠏⠀⠀⠀⠀⠀⠀⠀⠀⠀⡞⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⡇⠀⠀⠀
+⢀⡎⠀⢠⠇⠀⠀⠀⠀⠀⠀⠀⠀⠀⢸⠁⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⡆⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢸⠃⠀⠀⠀
+⢸⠀⠀⡎⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣸⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢸⠃⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢸⠀⠀⠀⠀
+⢸⡄⠀⡇⠀⠀⠀⠀⠀⠀⠀⠀⣀⡴⠋⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢸⠀⠀⠀⠀⠀⢀⠀⠀⡞⠀⠀⢸⠀⠀⠀⠀
+⠀⢇⠀⢱⡀⠀⠀⠀⠀⠀⡴⠋⠁⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢸⡀⠀⠀⠀⠀⣸⠀⣰⠃⠀⠀⢸⠀⠀⠀⠀
+⠀⠘⣆⠀⠹⣄⠀⠀⠀⠈⡇⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⡇⠀⠀⠀⢀⡇⢠⠇⡇⠀⠀⢼⠀⠀⠀⠀
+⠀⠀⠈⠣⡀⠀⠑⠦⣄⠀⠹⡄⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢻⠀⠀⠀⢸⡷⠃⠘⡇⠀⠀⠘⣇⠀⠀⠀
+⠀⠀⠀⠀⠈⠲⢤⣀⣀⣀⣀⣹⣦⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⡴⠀⠤⣤⡼⣇⠀⠀⠈⢷⡀⠀⢳⡀⠀⠀⠘⢳⡄⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠙⠲⠤⣀⣀⠀⠀⠀⢀⡼⠁⠀⠰⡌⢳⠿⡄⠀⠀⠀⠱⡄⠀⠳⢤⣀⣼⠾⠃⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠉⠉⠚⠉⠳⠤⠼⠿⠛⠉⠀⠳⣄⣰⣤⣷⠗⠀⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣈⣁⡄⠀⠀⠀⠀⠀⠀⠀⠀⠀
+*/
