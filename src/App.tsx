aa1725e37df125c728e433bd867259025ebe74de
Roma Artiukh <romanskarb@ukr.net>
2022-09-16 11:41:25 +0300

    comments were deleted

--- src/App.tsx
+++ src/App.tsx
@@ -4,26 +4,20 @@ type State = {
   key: string;
 };
 
-// export const App: React.FC = () => (
-//   <div className="App">
-//     <p className="App__message">The last pressed key is [Enter]</p>
-//   </div>
-// );
-
 export class App extends Component<{}, State> {
   state = {
     key: '',
   };
 
   componentDidMount() {
-    document.addEventListener('keyup', this.pressedKey);
+    document.addEventListener('keyup', this.keyPressed);
   }
 
   componentWillUnmount() {
-    document.removeEventListener('keyup', this.pressedKey);
+    document.removeEventListener('keyup', this.keyPressed);
   }
 
-  pressedKey = (event: KeyboardEvent) => {
+  keyPressed = (event: KeyboardEvent) => {
     if (event.key) {
       this.setState({ key: event.key });
     }
