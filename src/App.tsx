5f4503581f8d3ff0aabed91e3fa7ae4de7e043f2
Sergey Pistelev <sergey.pistelev@gmail.com>
2022-09-17 12:28:55 +0300

    add separate method for keyboardEvent

--- src/App.tsx
+++ src/App.tsx
@@ -10,16 +10,16 @@ export class App extends React.Component<{}, State> {
   };
 
   componentDidMount() {
-    document.addEventListener('keyup', (event: KeyboardEvent) => {
-      this.setState({ pressedKey: event.key });
-    });
+    document.addEventListener('keyup', this.pressedKeys);
   }
 
   componentWillUnmount() {
-    document.removeEventListener('keyup', (event: KeyboardEvent) => {
-      this.setState({ pressedKey: event.key });
-    });
+    document.removeEventListener('keyup', this.pressedKeys);
   }
+
+  pressedKeys = (event: KeyboardEvent) => {
+    this.setState({ pressedKey: event.key });
+  };
 
   render() {
     const { pressedKey } = this.state;
