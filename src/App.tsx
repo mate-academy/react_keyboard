7aefaffeafd91d135adf6af3e9456e93a5a9e4fd
Anton Novyk <Novyk.anton@gmail.com>
2022-10-03 11:05:09 +0300

    Remove comments

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,5 @@
 import React from 'react';
 
-/* export const App: React.FC = () => (
-  <div className="App">
-    <p className="App__message">The last pressed key is [Enter]</p>
-  </div>
-); */
-
 type State = {
   key: null | string
 };
