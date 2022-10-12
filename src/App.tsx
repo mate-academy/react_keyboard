2f1926d898548c9283d70a04e06cf6629eea26e3
Andrew Skoryk <andrewtimelessone@gmail.com>
2022-10-12 23:07:20 +0300

    feat: App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,37 @@
-import React from 'react';
+import { Component } from 'react';
 
-export const App: React.FC = () => (
-  <div className="App">
-    <p className="App__message">The last pressed key is [Enter]</p>
-  </div>
-);
+type State = {
+  key: string | null;
+};
+
+export class App extends Component<{}, State> {
+  state: Readonly<State> = {
+    key: '',
+  };
+
+  handleClick = (event: KeyboardEvent) => {
+    this.setState({ key: event.key });
+  };
+
+  componentDidMount = () => {
+    document.addEventListener('keyup', this.handleClick);
+  };
+
+  componentWillUnmount = () => {
+    document.removeEventListener('keyup', this.handleClick);
+  };
+
+  render() {
+    const { key } = this.state;
+
+    return (
+      <div className="App">
+        <p className="App__message">
+          {key
+            ? `The last pressed key is ${key}`
+            : 'Nothing was pressed yet'}
+        </p>
+      </div>
+    );
+  }
+}
