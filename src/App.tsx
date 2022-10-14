afc5202115f4d6b96b5a0135a15c67de8c82e39d
Serhii Romaniuk <deepspark@gmail.com>
2022-10-14 20:08:14 +0300

    replaced the function with a class, added active key display

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,35 @@
-import React from 'react';
+import { Component } from 'react';
 
-export const App: React.FC = () => (
-  <div className="App">
-    <p className="App__message">The last pressed key is [Enter]</p>
-  </div>
-);
+type State = {
+  activeKey: string | undefined;
+};
+
+export class App extends Component {
+  state: State = {
+    activeKey: undefined,
+  };
+
+  componentDidMount() {
+    document.addEventListener('keyup', this.keyClick);
+  }
+
+  componentWillUnmount() {
+    document.removeEventListener('keyup', this.keyClick);
+  }
+
+  keyClick = (event: KeyboardEvent) => (
+    this.setState({ activeKey: event.key })
+  );
+
+  render() {
+    return (
+      <div className="App">
+        <p className="App__message">
+          {!this.state.activeKey
+            ? 'Nothing was pressed yet'
+            : `The last pressed key is [${this.state.activeKey}]`}
+        </p>
+      </div>
+    );
+  }
+}
