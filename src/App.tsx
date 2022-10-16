191db6e9af2a566dad63803e2e82d7e3cb64ad01
Nick_Radchuk <mukola.radchuk@gmail.com>
2022-10-16 23:14:48 +0300

    solution

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,29 @@
-import React from 'react';
+import { Component } from 'react';
 
-export const App: React.FC = () => (
-  <div className="App">
-    <p className="App__message">The last pressed key is [Enter]</p>
-  </div>
-);
+type State = {
+  letter: string,
+};
+
+export class App extends Component<{}, State> {
+  state = {
+    letter: '',
+  };
+
+  componentDidMount() {
+    document.addEventListener('keyup', (event) => {
+      this.setState({ letter: event.key });
+    });
+  }
+
+  render() {
+    const { letter } = this.state;
+
+    return (
+      <div className="App">
+        <p className="App__message">
+          {`The last pressed key is [${letter}]`}
+        </p>
+      </div>
+    );
+  }
+}
