a1ff40852c90f5aadbc6ee65f5a3d1fba7025703
korniienkokarina <k.tashlytska@gmail.com>
2022-10-17 19:08:30 +0300

    keybord implemented v.1

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,37 @@
 import React from 'react';
 
-export const App: React.FC = () => (
-  <div className="App">
-    <p className="App__message">The last pressed key is [Enter]</p>
-  </div>
-);
+type State = {
+  keyPressed: string;
+};
+
+export class App extends React.Component<{}, State> {
+  state = {
+    keyPressed: '',
+  };
+
+  componentDidMount() {
+    document.addEventListener('keyup', this.handleKeyPressed);
+  }
+
+  componentWillUnmount() {
+    document.removeEventListener('keyup', this.handleKeyPressed);
+  }
+
+  handleKeyPressed = (event: KeyboardEvent) => {
+    this.setState({ keyPressed: event.key });
+  };
+
+  render() {
+    const key = this.state.keyPressed;
+
+    return (
+      <div className="App">
+        <p className="App__message">
+          {key
+            ? `The last pressed key is [${key}]`
+            : 'Nothing was pressed yet'}
+        </p>
+      </div>
+    );
+  }
+}
