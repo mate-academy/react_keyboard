dd91011050a2b42f4640dd0e594f1ab06057a79f
p-omogit-e <polinakotyrlo@gmail.com>
2022-10-25 18:35:56 +0300

    edited

--- src/App.tsx
+++ src/App.tsx
@@ -2,19 +2,23 @@ import React from 'react';
 
 export class App extends React.Component {
   state = {
-    str: 'Nothing was pressed yet',
+    pressedKey: undefined,
   };
 
   componentDidMount() {
     document.addEventListener('keyup', (event: KeyboardEvent) => {
-      this.setState({ str: `The last pressed key is ${event.key}` });
+      this.setState({ pressedKey: `The last pressed key is ${event.key}` });
     });
   }
 
   render() {
     return (
       <div className="App">
-        <p className="App__message">{this.state.str}</p>
+        <p className="App__message">
+          {this.state.pressedKey
+            ? this.state.pressedKey
+            : ('Nothing was pressed yet')}
+        </p>
       </div>
     );
   }
