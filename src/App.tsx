a6bad098a9c8158424b9d8b5d7d1ae0c342c9fe6
Yuliia Parashchenko <yuliia.parashchenko@gmail.com>
2022-11-10 00:28:02 +0200

    React keyboard task solution

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,31 @@
 import React from 'react';
 
-export const App: React.FC = () => (
-  <div className="App">
-    <p className="App__message">The last pressed key is [Enter]</p>
-  </div>
-);
+type State = {
+  pressedButton: string;
+};
+
+export class App extends React.Component {
+  state: Readonly<State> = {
+    pressedButton: '',
+  };
+
+  componentDidMount() {
+    document.addEventListener('keydown', (e) => {
+      const pressedButton = e.key;
+
+      this.setState({ pressedButton });
+    });
+  }
+
+  render() {
+    const { pressedButton } = this.state;
+
+    return (
+      <div className="App">
+        <p className="App__message">
+          { pressedButton ? `The last pressed key is [${pressedButton}]` : 'Nothing was pressed yet'}
+        </p>
+      </div>
+    );
+  }
+}
