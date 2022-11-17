2f278ca953f9665f3321a9b545b75da010269441
Max Skorokhod <mrzandras@gmail.com>
2022-11-17 19:34:46 +0200

    keboard

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,43 @@
-import React from 'react';
+import { Component } from 'react';
 
-export const App: React.FC = () => (
-  <div className="App">
-    <p className="App__message">The last pressed key is [Enter]</p>
-  </div>
-);
+type State = {
+  pressedKey: string;
+};
+
+export class App extends Component<{}, State> {
+  state: Readonly<State> = {
+    pressedKey: '',
+  };
+
+  componentDidMount() {
+    document.addEventListener('keyup', (this.getEventOfGlobalHandler));
+  }
+
+  componentWillUnmount() {
+    document.removeEventListener('keyup', (this.getEventOfGlobalHandler));
+  }
+
+  getEventOfGlobalHandler = (event: KeyboardEvent) => {
+    this.setState({ pressedKey: `${event.key}` });
+  };
+
+  render() {
+    const { pressedKey } = this.state;
+
+    return pressedKey.length
+      ? (
+        <div className="App">
+          <p className="App__message">
+            {`The last pressed key is [${pressedKey}]`}
+          </p>
+        </div>
+      )
+      : (
+        <div className="App">
+          <p className="App__message">
+            Nothing was pressed yet
+          </p>
+        </div>
+      );
+  }
+}
