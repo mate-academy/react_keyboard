942381ebd3de4787f7cbabdc92170b29277f9cac
viktor-kotik <vikuso1996@gmail.com>
2022-11-17 16:38:08 +0200

    Solution

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,36 @@
 import React from 'react';
 
-export const App: React.FC = () => (
-  <div className="App">
-    <p className="App__message">The last pressed key is [Enter]</p>
-  </div>
-);
+export class App extends React.Component {
+  state = {
+    pressedKey: '',
+  };
+
+  componentDidMount() {
+    document.addEventListener('keyup', (event: KeyboardEvent) => {
+      this.setState({ pressedKey: event.key });
+    });
+  }
+
+  componentWillUnmount() {
+    document.removeEventListener('keyup', () => {
+      this.setState({ pressedKey: '' });
+    });
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <p className="App__message">
+          {this.state.pressedKey
+            ? (
+              <>
+                {'The last pressed key is: '}
+                <b>{this.state.pressedKey}</b>
+              </>
+            )
+            : 'Nothing was pressed yet'}
+        </p>
+      </div>
+    );
+  }
+}
