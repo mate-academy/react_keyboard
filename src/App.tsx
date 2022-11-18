d073060b6cf7e3f213f41471505ea89aa17f4929
Anastasiia Tilikina <anastasiia.tilikina@gmail.com>
2022-11-18 19:30:37 +0200

    Implement keyboard stateful component

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,51 @@
-import React from 'react';
+import { Component } from 'react';
 
-export const App: React.FC = () => (
-  <div className="App">
-    <p className="App__message">The last pressed key is [Enter]</p>
-  </div>
-);
+// export const App: React.FC = () => (
+//   <div className="App">
+//     <p className="App__message">The last pressed key is [Enter]</p>
+//   </div>
+// );
+
+type Props = {
+
+};
+
+type State = {
+  pressedKey: string
+};
+
+export class App extends Component<Props, State> {
+  state: State = {
+    pressedKey: '',
+  };
+
+  initialMessage = 'Nothing was pressed yet';
+
+  updatedMessage = 'The last pressed key is';
+
+  componentDidMount() {
+    document.addEventListener('keyup', this.eventHandler);
+  }
+
+  componentWillUnmount() {
+    document.removeEventListener('keyup', this.eventHandler);
+  }
+
+  eventHandler = (e: KeyboardEvent) => {
+    this.setState({
+      pressedKey: e.key,
+    });
+  };
+
+  render() {
+    return (
+      <div className="App">
+        <p className="App__message">
+          {this.state.pressedKey.length === 0
+            ? this.initialMessage
+            : `${this.updatedMessage} [${this.state.pressedKey}]`}
+        </p>
+      </div>
+    );
+  }
+}
