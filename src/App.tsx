bfba9322ffb5a5997f0fd5f513300207b447a6b0
Yurii-Pozho <46326784+Yurii-Pozho@users.noreply.github.com>
2022-11-20 17:33:37 +0200

    react_ keyboard

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,27 @@
 import React from 'react';
 
-export const App: React.FC = () => (
-  <div className="App">
-    <p className="App__message">The last pressed key is [Enter]</p>
-  </div>
-);
+export class App extends React.Component {
+  state = {
+    currentChar: 'Nothing was pressed yet',
+  };
+
+  componentDidMount(): void {
+    document.addEventListener('keyup', (event: KeyboardEvent) => {
+      this.setState({ currentChar: `The last pressed key is [${event.key}]` });
+    });
+  }
+
+  componentWillUnmount(): void {
+    document.removeEventListener('keyup', () => {
+      this.setState({ currentChar: 'Nothing was pressed yet' });
+    });
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <p className="App__message">{this.state.currentChar}</p>
+      </div>
+    );
+  }
+}
