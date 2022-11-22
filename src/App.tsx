b216fa645807b0e48e34099a717a097a95a5d27e
Vadym Baranov <vadym.baranov@hotmail.com>
2022-11-22 15:11:48 +0200

    Keyboard tracker created

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,41 @@
-import React from 'react';
+import { Component } from 'react';
 
-export const App: React.FC = () => (
-  <div className="App">
-    <p className="App__message">The last pressed key is [Enter]</p>
-  </div>
-);
+type State = {
+  handleKeyPress: string
+};
+
+export class App extends Component<{}, State> {
+  state: Readonly<State> = {
+    handleKeyPress: '',
+  };
+
+  componentDidMount() {
+    document.addEventListener('keydown', this.onKeyDown);
+  }
+
+  componentWillUnmount() {
+    document.removeEventListener('keydown', this.onKeyDown);
+  }
+
+  onKeyDown = (event: KeyboardEvent) => {
+    const handleKey = event.key;
+
+    this.setState({ handleKeyPress: handleKey });
+  };
+
+  render() {
+    const { handleKeyPress: handleKey } = this.state;
+
+    return (
+      <div className="App">
+        <p className="App__message">
+          {
+            handleKey
+              ? `The last pressed key is [${handleKey}]`
+              : 'Nothing was pressed yet'
+          }
+        </p>
+      </div>
+    );
+  }
+}
