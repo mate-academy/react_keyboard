4e19dd351a0aa30b2914b36e7c975980f6459cfe
Vladimir Arkanov <vladimirarkanov@icloud.com>
2022-12-28 21:56:22 +0200

    first commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,48 @@
-import React from 'react';
+import { Component } from 'react';
+import Paper from '@mui/material/Paper';
+import Box from '@mui/material/Box';
 
-export const App: React.FC = () => (
-  <div className="App">
-    <p className="App__message">The last pressed key is [Enter]</p>
-  </div>
-);
+type State = {
+  pressedKey: string
+};
+
+export class App extends Component<{}, State> {
+  state:Readonly<State> = {
+    pressedKey: 'Nothing was pressed yet',
+  };
+
+  componentDidMount() {
+    document.addEventListener('keyup', (event: KeyboardEvent) => {
+      if (event.key) {
+        this.setState({ pressedKey: `The last pressed key is [${event.key}]` });
+      }
+    });
+  }
+
+  componentWillUnmount() {
+    document.removeEventListener('keyup', (event: KeyboardEvent) => {
+      if (event.key) {
+        this.setState({ pressedKey: `The last pressed key is [${event.key}]` });
+      }
+    });
+  }
+
+  render() {
+    const { pressedKey } = this.state;
+
+    return (
+      <div className="App">
+        <Box
+          className="App__message"
+          sx={{
+            width: 500,
+            height: 150,
+            backgroundColor: 'text.disabled',
+          }}
+        >
+          {pressedKey}
+        </Box>
+      </div>
+    );
+  }
+}
