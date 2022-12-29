52356d1b50e70e645799d9d11d173a58257d3dc6
Viktoriia Kravchenko <kravchenkovictoriya9@gmail.com>
2022-12-29 18:48:20 +0200

    Added App class component

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,47 @@
 import React from 'react';
 
-export const App: React.FC = () => (
-  <div className="App">
-    <p className="App__message">The last pressed key is [Enter]</p>
-  </div>
-);
+type State = {
+  pressedKey: string,
+};
+
+export class App extends React.Component<{}, State> {
+  state: Readonly<State> = {
+    pressedKey: '',
+  };
+
+  componentDidMount() {
+    document.addEventListener('keyup', this.handleKeyup);
+  }
+
+  handleKeyup = (event: KeyboardEvent) => {
+    let keyName = event.key;
+
+    if (keyName === ' ') {
+      keyName = 'space';
+    }
+
+    this.setState({ pressedKey: keyName });
+  };
+
+  commentWillUnmount() {
+    document.removeEventListener('keyup', this.handleKeyup);
+  }
+
+  render() {
+    const { pressedKey } = this.state;
+
+    return (
+      <div className="App">
+        {pressedKey ? (
+          <p className="App__message">
+            {`The last pressed key is [${pressedKey}]`}
+          </p>
+        ) : (
+          <p className="App__message">
+            Nothing was pressed yet
+          </p>
+        )}
+      </div>
+    );
+  }
+}
