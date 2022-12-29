f98feed8b84d8f987cfcf23954f5210b7c3ac324
Oleksandr Nikora <mailnik@ukr.net>
2022-12-30 00:42:50 +0100

    changes after mentor review

--- src/App.tsx
+++ src/App.tsx
@@ -27,11 +27,10 @@ export class App extends React.Component<{}, State> {
     return (
       <div className="App">
         <p className="App__message">
-          {
-            pressedKey
-              ? `The last pressed key is [${pressedKey}]`
-              : 'Nothing was pressed yet'
-          }
+
+          {pressedKey
+            ? `The last pressed key is [${pressedKey}]`
+            : 'Nothing was pressed yet'}
         </p>
       </div>
     );
