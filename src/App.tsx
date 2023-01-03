9a500d325f70b0c64d315a4e54337ec2083c9690
JRijo <99342891+John-Stiff@users.noreply.github.com>
2023-01-03 14:54:30 +0100

    implemented Class component

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,27 @@
 import React from 'react';
 
-export const App: React.FC = () => (
-  <div className="App">
-    <p className="App__message">The last pressed key is [Enter]</p>
-  </div>
-);
+type State = {
+  pressedKey: string;
+};
+
+export class App extends React.Component<{}, State> {
+  state = {
+    pressedKey: '',
+  };
+
+  render() {
+    const {
+      pressedKey,
+    } = this.state;
+
+    return (
+      <div className="App">
+        <p className="App__message">
+          {pressedKey
+            ? `The last pressed key is [${pressedKey}]`
+            : 'Nothing was pressed yet'}
+        </p>
+      </div>
+    );
+  }
+}
