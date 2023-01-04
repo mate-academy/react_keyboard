e3ff2ebb6bde609ebac76d1c4a54424dc0f724a3
Ala El Achkar <iamalaaashkar97@gmail.com>
2023-01-04 12:58:25 +0300

    edited some changes

--- src/App.tsx
+++ src/App.tsx
@@ -10,26 +10,24 @@ export class App extends React.Component<{}, State> {
   };
 
   componentDidMount() {
-    document.addEventListener('keyup', (event: KeyboardEvent) => {
-      this.setState({ pressedKey: (event.key) });
-    });
+    document.addEventListener('keyup', this.handlerCallback);
   }
 
   componentWillUnmount() {
-    document.removeEventListener('keyup', (event: KeyboardEvent) => {
-      this.setState({ pressedKey: (event.key) });
-    });
+    document.removeEventListener('keyup', this.handlerCallback);
   }
+
+  handlerCallback = (event: KeyboardEvent) => {
+    this.setState({ pressedKey: (event.key) });
+  };
 
   render() {
     return (
       <div className="App">
         <p className="App__message">
-          {this.state.pressedKey ? (
-            `The last pressed key is [${this.state.pressedKey}]`
-          ) : (
-            'Nothing was pressed yet'
-          )}
+          {this.state.pressedKey
+            ? `The last pressed key is [${this.state.pressedKey}]`
+            : 'Nothing was pressed yet'}
         </p>
       </div>
     );
