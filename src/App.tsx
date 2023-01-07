0284b77c24c9698ee8aea20fd5f78d97ce0d89b8
anton-sosonskyi <anton.sosonskyi@gmail.com>
2023-01-07 11:37:08 +0200

    fixed state

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,11 @@
 import { Component } from 'react';
 
 type State = {
-  keyPressed: boolean;
   key: string;
 };
 
 export class App extends Component<{}, State> {
   state: Readonly<State> = {
-    keyPressed: false,
     key: '',
   };
 
@@ -20,19 +18,18 @@ export class App extends Component<{}, State> {
   }
 
   handleKeyPressed = (event: KeyboardEvent) => {
-    this.setState({ keyPressed: true, key: event.key });
+    this.setState({ key: event.key });
   };
 
   render() {
-    const { key, keyPressed } = this.state;
+    const { key } = this.state;
 
     return (
       <div className="App">
         <p className="App__message">
-          {keyPressed ? (
-            `The last pressed key is [${key}]`
-          ) : ('Nothing was pressed yet'
-          )}
+          {key
+            ? `The last pressed key is [${key}]`
+            : 'Nothing was pressed yet'}
         </p>
       </div>
     );
