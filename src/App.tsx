5821e6af67273e93ecdf2744630ec018635d3d77
Kristina Makarovska <kris.makarovska@gmail.com>
2023-01-20 13:37:01 +0200

    Fixed 1

--- src/App.tsx
+++ src/App.tsx
@@ -10,16 +10,16 @@ export class App extends React.Component<{}, State> {
   };
 
   componentDidMount() {
-    document.addEventListener('keyup', (event: KeyboardEvent) => {
-      this.setState({ pressedKey: `The last pressed key is [${event.key}]` });
-    });
+    document.addEventListener('keyup', this.pressKey);
   }
 
   componentWillUnmount() {
-    document.removeEventListener('keyup', (event: KeyboardEvent) => {
-      this.setState({ pressedKey: `The last pressed key is [${event.key}]` });
-    });
+    document.removeEventListener('keyup', this.pressKey);
   }
+
+  pressKey = (event: KeyboardEvent) => {
+    this.setState({ pressedKey: `The last pressed key is [${event.key}]` });
+  };
 
   render() {
     const { pressedKey } = this.state;
