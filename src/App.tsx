759d601d679b6892547015a09fab85fbcfeefdc1
Olha Yevstifieieva <hob.bilbo@gmail.com>
2023-01-24 15:32:49 +0200

    Add react keybord App

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,35 @@
-import React from 'react';
+import { Component } from 'react';
 
-export const App: React.FC = () => (
-  <div className="App">
-    <p className="App__message">The last pressed key is [Enter]</p>
-  </div>
-);
+type State = {
+  messageChar: string;
+};
+
+export class App extends Component<{}, State> {
+  state = {
+    messageChar: 'Nothing was pressed yet',
+  };
+
+  componentDidMount() {
+    document.addEventListener('keyup', this.messageCharValue);
+  }
+
+  componentWillUnmount() {
+    document.removeEventListener('keyup', this.messageCharValue);
+  }
+
+  messageCharValue = (event: KeyboardEvent) => {
+    this.setState({ messageChar: `The last pressed key is [${event.key}]` });
+  };
+
+  render() {
+    const { messageChar } = this.state;
+
+    return (
+      <div className="App">
+        <p className="App__message">
+          {messageChar}
+        </p>
+      </div>
+    );
+  }
+}
