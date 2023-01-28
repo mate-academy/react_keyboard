2b8d8b8efba4b86bbbd178a1743826456f02e9d1
Kukharuk <its.up.to.you.dk@gmail.com>
2023-01-28 15:03:55 +0200

    keybord

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,35 @@
-import React from 'react';
+import { Component } from 'react';
 
-export const App: React.FC = () => (
-  <div className="App">
-    <p className="App__message">The last pressed key is [Enter]</p>
-  </div>
-);
+type State = {
+  previous: string,
+};
+
+export class App extends Component<{}, State> {
+  state = {
+    previous: '',
+  };
+
+  componentDidMount() {
+    document.addEventListener('keyup', this.handleDocClick);
+  }
+
+  componentWillUnmount() {
+    document.removeEventListener('keyup', this.handleDocClick);
+  }
+
+  handleDocClick = (ev: KeyboardEvent) => {
+    this.setState({ previous: ev.key });
+  };
+
+  render() {
+    return (
+      <div className="App">
+        <p className="App__message">
+          {this.state.previous
+            ? `The last pressed key is [${this.state.previous}]`
+            : 'Nothing was pressed yet'}
+        </p>
+      </div>
+    );
+  }
+}
