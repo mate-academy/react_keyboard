2f9ecc90674c3b1a95b203bef16bd111965fd798
Oleksandr Prykhodko <olexandr.prykhodko@gmail.com>
2023-01-29 14:08:21 +0100

    Class App__message were added

--- src/App.tsx
+++ src/App.tsx
@@ -24,7 +24,7 @@ export class App extends Component<{}, State> {
 
   render() {
     return (
-      <div>
+      <div className="App__message">
         <p>{this.state.pressedKey}</p>
       </div>
     );
