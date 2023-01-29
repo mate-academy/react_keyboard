649b36c6b6ab161e6bada4ae0ec3dc1d0276d27b
bart6man <igor.musson.55@gmail.com>
2023-01-29 14:22:36 +0200

    some fixes after strudent`s review

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,26 @@
 import React from 'react';
 
-export class App extends React.Component {
+interface State {
+  pressed: boolean,
+  key: null | string
+}
+
+export class App extends React.Component<{}, State> {
   state = {
     pressed: false,
     key: null,
   };
 
+  handelKey = (event: KeyboardEvent) => {
+    this.setState({ pressed: true, key: event.key });
+  };
+
   componentDidMount = () => {
-    document.addEventListener('keyup', (event) => {
-      this.setState({ pressed: true, key: event.key });
-    });
+    document.addEventListener('keyup', this.handelKey);
+  };
+
+  componentWillUnmount = () => {
+    document.removeEventListener('keyup', this.handelKey);
   };
 
   render() {
