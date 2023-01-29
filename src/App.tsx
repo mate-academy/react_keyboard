f09cc15f7c572223c8d1f2839a0cbb0c3e42143b
Oleksandr Prykhodko <olexandr.prykhodko@gmail.com>
2023-01-29 12:05:37 +0100

    React_Keyboard were implemented

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,31 @@
-import React from 'react';
+import { Component } from 'react';
 
-export const App: React.FC = () => (
-  <div className="App">
-    <p className="App__message">The last pressed key is [Enter]</p>
-  </div>
-);
+type State = {
+  pressedKey: string;
+};
+
+export class App extends Component<{}, State> {
+  state = {
+    pressedKey: 'Nothing was pressed yet',
+  };
+
+  componentDidMount() {
+    document.addEventListener('keyup', this.updatePressedKey);
+  }
+
+  componentWillUnmount() {
+    document.removeEventListener('keyup', this.updatePressedKey);
+  }
+
+  updatePressedKey = (event: KeyboardEvent) => {
+    this.setState({ pressedKey: `The last pressed key is [${event.key}]` });
+  };
+
+  render() {
+    return (
+      <div>
+        <p>{this.state.pressedKey}</p>
+      </div>
+    );
+  }
+}
