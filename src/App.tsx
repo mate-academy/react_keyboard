fc14b4360b28e968aefffe07d331faaf3593104b
Andrey Berezyanskiy <andrey.berezyanskiy@gmail.com>
2023-01-30 16:15:43 +0200

    useless variable was deleted

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,50 @@
-import React from 'react';
+import { Component } from 'react';
 
-export const App: React.FC = () => (
-  <div className="App">
-    <p className="App__message">The last pressed key is [Enter]</p>
-  </div>
-);
+type State = {
+  // isPressedKey: boolean
+  currentKey: string,
+};
+
+export class App extends Component<{}, State> {
+  state: Readonly<State> = {
+    // isPressedKey: false,
+    currentKey: '',
+  };
+
+  componentDidMount(): void {
+    document.addEventListener('keyup', this.handlePress);
+  }
+
+  componentWillUnmount(): void {
+    document.removeEventListener('keyup', this.handlePress);
+  }
+
+  handlePress = (event: KeyboardEvent) => {
+    this.setState({
+      // isPressedKey: true,
+      currentKey: event.key,
+    });
+  };
+
+  render() {
+    const { currentKey } = this.state;
+
+    return (
+      <div className="App">
+        {currentKey
+          ? (
+            <p className="App__message">
+              The last pressed key is [
+              {currentKey}
+              ]
+            </p>
+          )
+          : (
+            <p className="App__message">
+              Nothing was pressed yet
+            </p>
+          )}
+      </div>
+    );
+  }
+}
