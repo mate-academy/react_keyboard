8f5d3b19c61210ee0681af430e3bd3015d9b341d
Oleksandr-Maly <117357713+Oleksandr-Maly@users.noreply.github.com>
2023-01-30 16:58:59 +0200

    code refactored

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 type State = {
   message: string;
-}
+};
 
 export class App extends React.Component {
   state: Readonly<State> = {
@@ -10,15 +10,23 @@ export class App extends React.Component {
   };
 
   componentDidMount() {
-    document.addEventListener('keyup', (event: KeyboardEvent) => (
-      this.setState({ message: `The last pressed key is [${event.key}]` })
-    ));
+    document.addEventListener('keyup', this.handleKeyUp);
   }
 
+  componentWillUnmount() {
+    document.removeEventListener('keyup', this.handleKeyUp);
+  }
+
+  handleKeyUp = (event: KeyboardEvent) => (
+    this.setState({ message: `The last pressed key is [${event.key}]` })
+  );
+
   render() {
+    const { message } = this.state;
+
     return (
       <div className="App">
-        <p className="App__message">{this.state.message}</p>
+        <p className="App__message">{message}</p>
       </div>
     );
   }
