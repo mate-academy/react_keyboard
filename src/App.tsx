582aa0f15e208afc73890c3393a916b12cce23e6
Eugene P <eugenep@MacBook-Pro.local>
2023-02-01 15:54:26 +0200

    react keyboard v0.1

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,28 @@
 import React from 'react';
 
-export const App: React.FC = () => (
-  <div className="App">
-    <p className="App__message">The last pressed key is [Enter]</p>
-  </div>
-);
+export class App extends React.Component {
+  state = {
+    pressedKey: 'Nothing was pressed yet',
+  };
+
+  // eslint-disable-next-line react/sort-comp
+  handleDocumentClick = (event: KeyboardEvent) => {
+    this.setState({ pressedKey: `The last pressed key is [${event.key}]` });
+  };
+
+  componentDidMount() {
+    document.addEventListener('keyup', this.handleDocumentClick);
+  }
+
+  componentWillUnmount() {
+    document.removeEventListener('keyup', this.handleDocumentClick);
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <p className="App__message">{this.state.pressedKey}</p>
+      </div>
+    );
+  }
+}
