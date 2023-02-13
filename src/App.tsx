6d750274dc7a10623abb09f914b65ce80eb5538b
Alex Vahnovan <sonyaxent@gmail.com>
2023-02-13 17:31:18 +0100

    added keyboard events

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,35 @@
-import React from 'react';
+import { Component } from 'react';
 
-export const App: React.FC = () => (
-  <div className="App">
-    <p className="App__message">The last pressed key is [Enter]</p>
-  </div>
-);
+type State = {
+  newKey: string;
+};
+
+export class App extends Component<{}, State> {
+  state: Readonly<State> = {
+    newKey: '',
+  };
+
+  componentDidMount() {
+    document.addEventListener('keyup', this.eventCallback);
+  }
+
+  componentWillUnmount() {
+    document.removeEventListener('keyup', this.eventCallback);
+  }
+
+  eventCallback = (ev: KeyboardEvent) => {
+    this.setState({ newKey: ev.key });
+  };
+
+  render() {
+    const { newKey } = this.state;
+
+    return (
+      <div className="App">
+        <p className="App__message">
+          {(newKey && `The last pressed key is [${newKey}]`) || 'Nothing was pressed yet'}
+        </p>
+      </div>
+    );
+  }
+}
