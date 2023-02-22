0e7d77f7bda3483d1484da2b17185b2a36852c57
Oksana Shvets <ksenyasvatko1993@gmail.com>
2023-02-22 16:22:54 +0100

    Solution

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,41 @@
-import React from 'react';
+// import React from 'react';
+import { Component } from 'react';
 
-export const App: React.FC = () => (
-  <div className="App">
-    <p className="App__message">The last pressed key is [Enter]</p>
-  </div>
-);
+type State = {
+  pressedKey: string;
+};
+
+export class App extends Component<{}, State> {
+  constructor(props: {}) {
+    super(props);
+
+    // eslint-disable-next-line react/state-in-constructor
+    this.state = {
+      pressedKey: '',
+    };
+
+    this.handleKeyUp = this.handleKeyUp.bind(this);
+  }
+
+  componentDidMount() {
+    document.addEventListener('keyup', this.handleKeyUp);
+  }
+
+  componentWillUnmount() {
+    document.removeEventListener('keyup', this.handleKeyUp);
+  }
+
+  handleKeyUp(event: KeyboardEvent) {
+    this.setState({ pressedKey: event.key });
+  }
+
+  render() {
+    const { pressedKey } = this.state;
+
+    return (
+      <div className="App">
+        <p className="App__message">{pressedKey ? `The last pressed key is [${pressedKey}]` : 'Nothing was pressed yet'}</p>
+      </div>
+    );
+  }
+}
