67b994c7fa4d4438f2121a7be1e42e765c1595ec
Oleksii Pishchuhin <aliliolek@gmail.com>
2023-03-09 01:53:28 +0100

    sol 1.0

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,37 @@
-import React from 'react';
+import { Component } from 'react';
 
-export const App: React.FC = () => (
-  <div className="App">
-    <p className="App__message">The last pressed key is [Enter]</p>
-  </div>
-);
+type State = {
+  keyName: string;
+};
+
+export class App extends Component<{}, State> {
+  state = {
+    keyName: '',
+  };
+
+  componentDidMount(): void {
+    document.addEventListener('keyup', this.handleDocumentClick);
+  }
+
+  componentWillUnmount(): void {
+    document.removeEventListener('keyup', this.handleDocumentClick);
+  }
+
+  handleDocumentClick = (event: KeyboardEvent) => {
+    this.setState({ keyName: event.key });
+  };
+
+  render() {
+    return (
+      <div className="App">
+        <p className="App__message">
+          {this.state.keyName ? (
+            `The last pressed key is [${this.state.keyName}]`
+          ) : (
+            'Nothing was pressed yet'
+          )}
+        </p>
+      </div>
+    );
+  }
+}
