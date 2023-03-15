a7b606a20e5527eb2f12edbb1426b560b68875d2
Vladyslav Kanaiev <nordex83@gmail.com>
2023-03-15 11:04:46 +0200

    solution

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,8 @@
 import React from 'react';
+import { Keyboard } from './keyboard';
 
 export const App: React.FC = () => (
   <div className="App">
-    <p className="App__message">The last pressed key is [Enter]</p>
+    <p className="App__message"><Keyboard /></p>
   </div>
 );
