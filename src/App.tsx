334c413ed7f986ec0018ac2c5ce00607893d3e8c
Vladyslav Kanaiev <nordex83@gmail.com>
2023-03-15 16:36:13 +0200

    fixes

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,39 @@
-import React from 'react';
-import { Keyboard } from './keyboard';
+import { Component, ReactNode } from 'react';
 
-export const App: React.FC = () => (
-  <div className="App">
-    <p className="App__message"><Keyboard /></p>
-  </div>
-);
+type State = {
+  key: string,
+};
+
+export class App extends Component<{}, State> {
+  state: Readonly<State> = {
+    key: '',
+  };
+
+  componentDidMount() {
+    document.addEventListener('keyup', this.saveKey);
+  }
+
+  componentWillUnmount() {
+    document.removeEventListener('keyup', this.saveKey);
+  }
+
+  saveKey = (event: KeyboardEvent) => {
+    this.setState({ key: event.key });
+  };
+
+  render(): ReactNode {
+    const { key } = this.state;
+
+    return (
+      <div className="App">
+        <p className="App__message">
+          {
+            key
+              ? `The last pressed key is [${key}]`
+              : 'Nothing was pressed yet'
+          }
+        </p>
+      </div>
+    );
+  }
+}
