56613ebc634fd7fa34a6cbef82a57dfee20d8794
Mykola Serbin <serbin.nick@gmail.com>
2023-04-10 21:36:46 +0200

    fixed []

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,11 @@
 import { Component } from 'react';
 
-export class App extends Component {
-  state = {
+type State = {
+  key: string;
+}
+
+export class App extends Component<{}, State> {
+  state: Readonly<State> = {
     key: '',
   };
 
@@ -23,7 +27,7 @@ export class App extends Component {
     return (
       <div className="App">
         <p className="App__message">
-          { key ? `The last pressed key is ${key}` : "Nothing was pressed yet"}
+          { key !== '' ? `The last pressed key is [${key}]` : 'Nothing was pressed yet'}
         </p>
       </div>
     );
