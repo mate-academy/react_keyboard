e1e36452d8b198a516fe9cd69336ac80018c93ee
glots-a <glots@ukr.net>
2023-04-14 15:46:53 +0300

    react keybord

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,53 @@
 import React from 'react';
 
-export const App: React.FC = () => (
-  <div className="App">
-    <p className="App__message">The last pressed key is [Enter]</p>
-  </div>
-);
+type State = {
+  wasPresed: boolean;
+  keyPres: null | string;
+};
+
+export class App extends React.Component<{}, State> {
+  state = {
+    wasPresed: false,
+    keyPres: null,
+  };
+
+  componentDidMount(): void {
+    document.addEventListener('keyup', (e: KeyboardEvent) => {
+      this.setState({
+        wasPresed: true,
+        keyPres: e.key,
+      });
+    });
+  }
+
+  componentWillUnmount(): void {
+    document.addEventListener('keyup', (e: KeyboardEvent) => {
+      this.setState({
+        wasPresed: true,
+        keyPres: e.key,
+      });
+    });
+  }
+
+  // handlePressKey(e: KeyboardEvent) {
+  //   this.setState({
+  //     wasPresed: true,
+  //     keyPres: e.key,
+  //   });
+  // }
+  // => then I used  handlePressKey methot I got an error.
+
+  render() {
+    return (
+      <div className="App">
+        <p className="App__message">
+          {
+            this.state.wasPresed
+              ? `The last pressed key is [${this.state.keyPres}]`
+              : 'Nothing was pressed yet'
+          }
+        </p>
+      </div>
+    );
+  }
+}
