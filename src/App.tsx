66599c3e6d6129e629d8f2eaaa728d3dca62fb2b
kukla1989 <kukla.1989@ukr.net>
2023-04-14 17:04:27 +0300

    solve task react_keyboard

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,41 @@
 import React from 'react';
 
+interface State {
+  key: string;
+}
+
+export class WhatIsPressed extends React.Component<{}, State> {
+  state = {
+    key: 'Meta',
+  };
+
+  componentDidMount() {
+    document.addEventListener('keyup', this.handleKeypress);
+  }
+
+  componentWillUnmount() {
+    document.removeEventListener('keyup', this.handleKeypress);
+  }
+
+  handleKeypress = (event: KeyboardEvent) => {
+    this.setState({ key: event.key });
+  };
+
+  render() {
+    const { key } = this.state;
+
+    return (
+      <p className="App__message">
+        {key === 'Meta'
+          ? 'Nothing was pressed yet'
+          : `The last pressed key is [${key}]`}
+      </p>
+    );
+  }
+}
+
 export const App: React.FC = () => (
   <div className="App">
-    <p className="App__message">The last pressed key is [Enter]</p>
+    <WhatIsPressed />
   </div>
 );
