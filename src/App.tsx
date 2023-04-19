64b3b0ebf9ee160c23db479654ba81a8376bb014
Rostyslav Hirin <hirin.rostyslav@gmail.com>
2023-04-19 10:59:43 +0300

    Code style improvements

--- src/App.tsx
+++ src/App.tsx
@@ -1,31 +1,37 @@
 import React from 'react';
 
 interface State {
-  pressedKey: string;
+  pressedKey: string | null;
 }
 
 export class App extends React.Component<{}, State> {
   state = {
-    pressedKey: 'Nothing was pressed yet',
+    pressedKey: null,
   };
 
-  componentDidMount(): void {
+  componentDidMount() {
     window.addEventListener('keydown', this.handleKeyboardPress);
   }
 
-  componentWillUnmount(): void {
+  componentWillUnmount() {
     window.removeEventListener('keydown', this.handleKeyboardPress);
   }
 
-  handleKeyboardPress = (event: KeyboardEvent): void => {
-    this.setState({ pressedKey: `The last pressed key is [${event.key}]` });
+  handleKeyboardPress = (event: KeyboardEvent) => {
+    this.setState({ pressedKey: event.key });
   };
 
   render() {
+    const { pressedKey } = this.state;
+
     return (
       <div className="App">
         <p className="App__message">
-          {this.state.pressedKey}
+          {
+            pressedKey
+              ? `The last pressed key is [${pressedKey}]`
+              : 'Nothing was pressed yet'
+          }
         </p>
       </div>
     );
