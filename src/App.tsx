46ae601acfd2f07fc1bf7b470684ae00140c55e3
Edgar Kocuba <edgarkocuba@gmail.com>
2023-04-20 09:26:54 +0300

    function added

--- src/App.tsx
+++ src/App.tsx
@@ -4,33 +4,33 @@ type State = {
   pressedKey: string | null;
 };
 
-export class App extends React.Component<State> {
-  state = {
+export class App extends React.Component<{}, State> {
+  state:Readonly<State> = {
     pressedKey: null,
   };
 
-  componentDidMount() {
-    document.addEventListener('keyup', (event: KeyboardEvent) => {
-      this.setState({ pressedKey: event.key });
-    });
+  componentDidMount(): void {
+    document.addEventListener('keyup', this.keyPressed);
   }
 
-  componentWillUnmount() {
-    document.removeEventListener('keyup', (event: KeyboardEvent) => {
-      this.setState({ pressedKey: event.key });
-    });
+  componentWillUnmount(): void {
+    document.removeEventListener('keyup', this.keyPressed);
   }
+
+  keyPressed = (event: KeyboardEvent) => {
+    this.setState({ pressedKey: event.key });
+  };
 
   render() {
     const { pressedKey } = this.state;
 
-    const message = pressedKey
-      ? `The last pressed key is [${pressedKey}]`
-      : 'Nothing was pressed yet';
-
     return (
       <div className="App">
-        <p className="App__message">{message}</p>
+        <p className="App__message">
+          {pressedKey
+            ? `The last pressed key is [${pressedKey}]`
+            : 'Nothing was pressed yet'}
+        </p>
       </div>
     );
   }
