7bfc406b256c1b8d79372eb117f9e988dbe5981b
Barbara Jeziorek <barbarajeziorek@gmail.com>
2023-05-05 09:15:01 +0200

    solution v1

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,33 @@
 import React from 'react';
 
+class KeyListener extends React.Component {
+  state = {
+    pressedKey: '',
+  };
+
+  introText = 'The last pressed key is [';
+
+  componentDidMount(): void {
+    document.addEventListener('keyup', this.handleKeyPress);
+  }
+
+  componentWillUnmount(): void {
+    document.removeEventListener('keyup', this.handleKeyPress);
+  }
+
+  handleKeyPress = (event: KeyboardEvent) => {
+    this.setState({ pressedKey: `${this.introText + event.key}]` });
+  };
+
+  render(): React.ReactNode {
+    return <>{`${this.state.pressedKey || 'Nothing was pressed yet'}`}</>;
+  }
+}
+
 export const App: React.FC = () => (
   <div className="App">
-    <p className="App__message">The last pressed key is [Enter]</p>
+    <p className="App__message">
+      <KeyListener />
+    </p>
   </div>
 );
