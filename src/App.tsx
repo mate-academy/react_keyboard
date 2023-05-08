0d971ae2c20812f64ce4d6582bab1f1189fe9e45
catgode <105614630+catgode@users.noreply.github.com>
2023-05-08 23:12:24 +0200

    solve 1

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,35 @@
-import React from 'react';
+/* eslint-disable no-console */
+import React, { Key } from 'react';
 
-export const App: React.FC = () => (
-  <div className="App">
-    <p className="App__message">The last pressed key is [Enter]</p>
-  </div>
-);
+type State = {
+  pressedKey: Key | null;
+};
+
+export class App extends React.Component<{}, State> {
+  state: State = {
+    pressedKey: null,
+  };
+
+  componentDidMount() {
+    document.addEventListener('keyup', (event: KeyboardEvent) => {
+      console.log(event.key);
+      this.setState({ pressedKey: event.key });
+    });
+  }
+
+  componentWillUnmount() {
+    document.removeEventListener('keyup', (event: KeyboardEvent) => {
+      console.log(event.key);
+    });
+  }
+
+  render() {
+    const { pressedKey } = this.state;
+
+    if (pressedKey) {
+      return <p>{`The last pressed key is [${pressedKey}]`}</p>;
+    }
+
+    return <p>Nothing was pressed yet</p>;
+  }
+}
