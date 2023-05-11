e1ae08f1c8a601b90abc3677230b6578933bb52e
anton_chornobai <antonchornobajj@gmail.com>
2023-05-11 15:42:43 +0200

    task was done

--- src/App.tsx
+++ src/App.tsx
@@ -2,28 +2,31 @@ import React from 'react';
 
 export class App extends React.Component {
   state = {
-    message: 'Nothing was pressed yet',
+    message: null,
   };
 
   componentDidMount() {
-    document.addEventListener('keyup', (event: KeyboardEvent) => {
-      this.setState({ message: `The last pressed key is [${event.key}]` });
-    });
+    document.addEventListener('keyup', this.handleOnKeyUp);
   }
 
   componentWillUnmount() {
-    document.removeEventListener('keyup', (event: KeyboardEvent) => {
-      this.setState({ message: `The last pressed key is [${event.key}]` });
-    });
+    document.removeEventListener('keyup', this.handleOnKeyUp);
   }
 
-  render(): JSX.Element {
+  handleOnKeyUp = (event: KeyboardEvent) => {
+    this.setState({ message: event.key });
+  }
+
+  render() {
     const { message } = this.state;
 
     return (
       <div className="App">
         <p className="App__message">
-          {message}
+          {message
+           ? `The last pressed key is [${message}]`
+           : `Nothing was pressed yet`
+          } 
         </p>
       </div>
     );
