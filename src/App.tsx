e604e95dd08dbc80129905c2531b2766c94ecd4c
Olena Shevchenko <shevochkas@gmail.com>
2023-05-25 18:45:22 +0300

    react class was added

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,43 @@
 import React from 'react';
 
-export const App: React.FC = () => (
+type State = {
+  pressedKey: string | null;
+};
+
+export class App extends React.Component<{}, State> {
+  state = {
+    pressedKey: null,
+  };
+
+  componentDidMount() {
+    document.addEventListener('keyup', this.handlerKeyPress);
+  }
+
+  componentWillUnmount() {
+    document.removeEventListener('keyup', this.handlerKeyPress);
+  }
+
+  handlerKeyPress = (event: KeyboardEvent) => {
+    this.setState({ pressedKey: event.key });
+  };
+
+  render() {
+    const { pressedKey } = this.state;
+
+    return (
+      <div className="App">
+        <p className="App__message">
+          {pressedKey
+            ? `The last pressed key is [${pressedKey}]`
+            : 'Nothing was pressed yet'}
+        </p>
+      </div>
+    );
+  }
+}
+
+/* export const App: React.FC = () => (
   <div className="App">
     <p className="App__message">The last pressed key is [Enter]</p>
   </div>
-);
+); */
