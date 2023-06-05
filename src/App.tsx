6572394259295455c4f7b9b03791438259475d7a
Victor Buldenko <gookidoo@gmail.com>
2023-06-05 18:24:11 +0300

    rework

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,35 @@
-import React from 'react';
-import { Appmessage } from './Appmessage/Appmessage';
+import { Component } from 'react';
 
-export const App: React.FC = () => (
-  <div className="App">
-    <Appmessage />
-  </div>
-);
+export class App extends Component {
+  state = {
+    key: null,
+  };
+
+  message = 'Nothing was pressed yet';
+
+  componentDidMount(): void {
+    document.addEventListener('keyup', this.keyboardHandler);
+  }
+
+  componentWillUnmount(): void {
+    document.removeEventListener('keyup', this.keyboardHandler);
+  }
+
+  keyboardHandler = (event: KeyboardEvent): void => {
+    const x = event.key === ' ' ? 'Space' : event.key;
+
+    this.setState({ key: x });
+  };
+
+  render() {
+    return (
+      <div className="App">
+        <p className="App__message">
+          {this.state.key
+            ? `The last pressed key is [${this.state.key}]`
+            : 'Nothing was pressed yet'}
+        </p>
+      </div>
+    );
+  }
+}
