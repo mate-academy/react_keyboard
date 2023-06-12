94b59cca42945a8b6932f5142aa0e436b5e1bc68
Dima-Petrov <danielgromph@gmail.com>
2023-06-12 20:43:10 +0300

    fixed after code review

--- src/App.tsx
+++ src/App.tsx
@@ -1,36 +1,38 @@
 import { Component } from 'react';
 
 type State = {
-  pressedKey: string;
+  pressedKey: string | null;
 };
 
 export class App extends Component<{}, State> {
   state: State = {
-    pressedKey: 'Nothing was pressed yet',
+    pressedKey: null,
   };
 
   componentDidMount() {
-    document.addEventListener('keyup', (event: KeyboardEvent) => {
-      this.setState({
-        pressedKey: `The last pressed key is [${event.key}]`,
-      });
-    });
+    document.addEventListener('keyup', this.keyStateHandler);
   }
 
   componentWillUnmount() {
-    document.removeEventListener('keyup', (event: KeyboardEvent) => {
-      this.setState({
-        pressedKey: `The last pressed key is [${event.key}]`,
-      });
-    });
+    document.removeEventListener('keyup', this.keyStateHandler);
   }
+
+  keyStateHandler = (event: KeyboardEvent) => {
+    this.setState({
+      pressedKey: `The last pressed key is [${event.key}]`,
+    });
+  };
 
   render() {
     const { pressedKey } = this.state;
 
     return (
       <div className="App">
-        <p className="App__message">{ pressedKey }</p>
+        <p className="App__message">
+          { !pressedKey
+            ? 'Nothing was pressed yet'
+            : pressedKey}
+        </p>
       </div>
     );
   }
