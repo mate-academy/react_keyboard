87be478c727b7cd4766b05e4590a4e0da62ed0d7
IvanVaverchak <ivanvaverchak@2000gmail.com>
2023-06-19 15:38:48 +0300

    remove listener

--- src/App.tsx
+++ src/App.tsx
@@ -11,6 +11,12 @@ export class App extends React.Component {
     });
   }
 
+  componentWillUnmount() {
+    document.removeEventListener('keyup', (e: KeyboardEvent) => {
+      this.setState({ pressedKey: e.key });
+    });
+  }
+
   render() {
     const { pressedKey } = this.state;
 
