b7384fdfa095c90f2d3caab85eb0dce91430dccd
Skrudj <ssinilov@gmail.com>
2023-07-10 19:05:22 +0300

    implement App component using eventListener

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,27 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 
-export const App: React.FC = () => (
-  <div className="App">
-    <p className="App__message">The last pressed key is [Enter]</p>
-  </div>
-);
+export const App: React.FC = () => {
+  const [key, setKey] = useState<string | null>(null);
+
+  const keyEventHandler = (event: KeyboardEvent) => {
+    setKey(event.key);
+  };
+
+  useEffect(() => {
+    document.addEventListener('keyup', keyEventHandler);
+
+    return () => {
+      document.removeEventListener('keyup', keyEventHandler);
+    };
+  }, []);
+
+  return (
+    <div className="App">
+      {key ? (
+        <p className="App__message">{`The last pressed key is [${key}]`}</p>
+      ) : (
+        <p className="App_message">Nothing was pressed yet</p>
+      )}
+    </div>
+  );
+};
