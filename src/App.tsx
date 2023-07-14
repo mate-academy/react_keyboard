495f223c95a9b839cd3abe0d16d5c1e1c9cc2f00
Nazarii Piatnochka <nazar.piatnochka@gmail.com>
2023-07-14 14:52:50 +0300

    Some fixes

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,7 @@ type State = {
   lastKeyPressed: string
 };
 
-export class App extends React.Component<State> {
+export class App extends React.Component {
   state: State = {
     lastKeyPressed: '',
   };
