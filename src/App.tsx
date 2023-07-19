0e08a0085c9c50dbebc2f61e75a51c05ba5e0fde
Eugene Kulish <genekulish13@gmail.com>
2023-07-19 15:03:55 +0300

    add keyboard

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,37 @@
 import React from 'react';
 
-export const App: React.FC = () => (
-  <div className="App">
-    <p className="App__message">The last pressed key is [Enter]</p>
-  </div>
-);
+import { AppAsFC } from './AppAsFC/AppAsFC';
+
+export class App extends React.Component {
+  state = {
+    key: '',
+  };
+
+  componentDidMount(): void {
+    document.addEventListener('keyup', this.handleDocumentKeyPress);
+  }
+
+  componentWillUnmount(): void {
+    document.removeEventListener('keyup', this.handleDocumentKeyPress);
+  }
+
+  handleDocumentKeyPress = (event: KeyboardEvent) => {
+    this.setState({ key: event.key });
+  };
+
+  render() {
+    return (
+      <>
+        <div className="App">
+          <p className="App__message">
+            {(this.state.key !== '')
+              ? (`The last pressed key is [${this.state.key}]`)
+              : ('Nothing was pressed yet')}
+          </p>
+        </div>
+
+        <AppAsFC />
+      </>
+    );
+  }
+}
