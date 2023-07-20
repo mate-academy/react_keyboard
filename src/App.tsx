512f27e0abb8e048a2fb61821270d169ba962ca3
Oleh Kandaskalov <olegkandaskalov@gmail.com>
2023-07-20 23:30:36 +0300

    add task solution

--- src/App.tsx
+++ src/App.tsx
@@ -28,7 +28,7 @@ export class App extends React.Component<{}, State> {
       <div className="App">
         <p className="App__message">
           {pressedKey
-            ? `The last pressed key is ${pressedKey}`
+            ? `The last pressed key is ${[pressedKey]}`
             : 'Nothing was pressed yet'}
         </p>
       </div>
