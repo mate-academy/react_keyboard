978e077d118f44bcd6a05c9a1e1bb049538a6b07
AndriiYelieva <andrew.yelieva@gmail.com>
2023-07-21 17:36:04 +0300

    add task sourion

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,33 @@
 import React from 'react';
 
-export const App: React.FC = () => (
-  <div className="App">
-    <p className="App__message">The last pressed key is [Enter]</p>
-  </div>
-);
+type State = {
+  pressedKey: string;
+};
+
+export class App extends React.Component<{}, State> {
+  state: State = {
+    pressedKey: 'Nothing was pressed yet',
+  };
+
+  componentDidMount(): void {
+    document.addEventListener('keydown', this.handKeyCLick);
+  }
+
+  componentWillUnmount() {
+    document.removeEventListener('keyup', this.handKeyCLick);
+  }
+
+  handKeyCLick = (event: KeyboardEvent) => {
+    this.setState({ pressedKey: `The last pressed key is [${event.key}]` });
+  };
+
+  render() {
+    const { pressedKey } = this.state;
+
+    return (
+      <div>
+        <p>{pressedKey}</p>
+      </div>
+    );
+  }
+}
