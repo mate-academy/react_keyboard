8883795efaede6f2ae53d0928e72ac06db649442
domkab <dominykas.bobinas@gmail.com>
2023-08-07 14:08:14 +0300

    1.0.1

--- src/App.tsx
+++ src/App.tsx
@@ -25,8 +25,6 @@ export class App extends React.Component<{}, State> {
     return (
       <div className="App">
         <p className="App__message">
-          The last pressed key is
-          {' '}
           {`${this.state.lastKey}`}
         </p>
       </div>
