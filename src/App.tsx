5ccb8059a354c543f34975f469f299bfee7b8b21
Horizon-git <apbreloaded947@gmail.com>
2023-08-21 22:57:36 +0300

    completed task

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,35 @@
 import React from 'react';
 
-export const App: React.FC = () => (
-  <div className="App">
-    <p className="App__message">The last pressed key is [Enter]</p>
-  </div>
-);
+type State = {
+  key: string,
+};
+// eslint-disable-next-line react/prefer-stateless-function
+export class App extends React.Component {
+  state: State = {
+    key: '',
+  };
+
+  componentDidMount() {
+    document.addEventListener('keyup', this.handleKeyPress);
+  }
+
+  componentWillUnmount() {
+    document.removeEventListener('keyup', this.handleKeyPress);
+  }
+
+  handleKeyPress = (event: KeyboardEvent) => {
+    this.setState({ key: event.key });
+  };
+
+  render() {
+    return (
+      <div className="App">
+        <p className="App__message">
+          {this.state.key
+            ? `The last pressed key is ${this.state.key}`
+            : 'Nothing was pressed yet'}
+        </p>
+      </div>
+    );
+  }
+}
