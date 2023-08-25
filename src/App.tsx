35fe8057c1eeb1edba2dd22bc0a2c9597da38e59
Andrzej Pietkiewicz <pietan@me.com>
2023-08-25 13:12:52 +0200

    small correction in returned string

--- src/App.tsx
+++ src/App.tsx
@@ -26,7 +26,7 @@ export class App extends React.Component {
       <div className="App">
         {pressedKey === ''
           ? <p className="App__message">Nothing was pressed yet</p>
-          : <p className="App__message">{`The last pressed key is [ ${pressedKey} ]`}</p>}
+          : <p className="App__message">{`The last pressed key is [${pressedKey}]`}</p>}
       </div>
     );
   }
