4eb2bbf1725f457982049dd9a59305c8d4daffcb
Vitalii Fedusov <vitalii.fedusov@gmail.com>
2023-08-26 15:32:58 +0300

    my solution

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,28 @@
-import React from 'react';
-// eslint-disable-next-line max-len
-import { LastPressedKey2 } from './ClassComponent/LastPressedKey2/LastPressedKey2';
-import { LastPressedKey } from './FunctionalComponent/LastPressedKey';
+import { useEffect, useState } from 'react';
 
-export const App: React.FC = () => (
-  <div className="App">
-    <LastPressedKey2 />
-    <LastPressedKey />
-  </div>
-);
+export const App: React.FC = () => {
+  const [key, setKey] = useState('');
+
+  useEffect(() => {
+    const handleKeyPress = (event: KeyboardEvent) => {
+      setKey(event.key);
+      // console.log(event.key);
+    };
+
+    document.addEventListener('keyup', handleKeyPress);
+
+    return () => {
+      document.removeEventListener('keyup', handleKeyPress);
+    };
+  });
+
+  return (
+    <>
+      {key ? (
+        <div className="App__message">{`The last pressed key is [${key}]`}</div>
+      ) : (
+        <div className="App__message">Nothing was pressed yet</div>
+      )}
+    </>
+  );
+};
