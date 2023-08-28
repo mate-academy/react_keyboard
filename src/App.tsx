d7c1a491b7fb6c5d8a3c8ab7ff183b8de66c418c
Matylda Lewandowska <matylda.lewandowska111@gmail.com>
2023-08-28 15:32:15 +0200

    adjustments has been made

--- src/App.tsx
+++ src/App.tsx
@@ -18,21 +18,17 @@ export class App extends React.Component<{}, State> {
   }
 
   handleKeyUp = (event: KeyboardEvent) => {
-    this.setState({ pressedKey: event.key });
+    this.setState({ pressedKey: `The last pressed key is [${event.key}]` });
   };
 
   render() {
     const { pressedKey } = this.state;
 
-    let appMessage = <p className="App__message">Nothing was pressed yet</p>;
-
-    if (pressedKey !== 'Nothing was pressed yet') {
-      appMessage = <p className="App__message">{`The last pressed key is [${pressedKey}]`}</p>;
-    }
-
     return (
       <div className="App">
-        {appMessage}
+        <p className="App__message">
+          {pressedKey}
+        </p>
       </div>
     );
   }
