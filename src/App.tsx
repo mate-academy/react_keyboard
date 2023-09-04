a3b715f86c46f9248e7ee9bd60bc43c34b94ed56
Marzena Sówka <marzena.sowka.pl@gmail.com>
2023-09-04 15:32:32 +0200

    add keypress handler

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,37 @@
 import React from 'react';
 
-export const App: React.FC = () => (
-  <div className="App">
-    <p className="App__message">The last pressed key is [Enter]</p>
-  </div>
-);
+type State = {
+  pressedKey: string | null;
+};
+
+export class App extends React.Component<{}, State> {
+  state: Readonly<State> = {
+    pressedKey: null,
+  };
+
+  componentDidMount() {
+    document.addEventListener('keyup', this.handleKeyup);
+  }
+
+  componentWillUnmount() {
+    document.removeEventListener('keyup', this.handleKeyup);
+  }
+
+  handleKeyup = (event: KeyboardEvent) => {
+    this.setState({ pressedKey: event.key });
+  };
+
+  render() {
+    const content = !this.state.pressedKey
+      ? 'Nothing was pressed yet'
+      : `The last pressed key is [${this.state.pressedKey}]`;
+
+    return (
+      <div className="App">
+        <p className="App__message">
+          {content}
+        </p>
+      </div>
+    );
+  }
+}
