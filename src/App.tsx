cfde63e2b6da54977cf992ff53829aeb2410ffd9
daviinnchi <zaikoivan99@gmail.com>
2023-09-07 19:02:23 +0200

    implemented logic for printing a last pressed button

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,27 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 
-export const App: React.FC = () => (
-  <div className="App">
-    <p className="App__message">The last pressed key is [Enter]</p>
-  </div>
-);
+export const App: React.FC = () => {
+  const [key, setKey] = useState('');
+
+  const handleKeyPress = (event: KeyboardEvent) => {
+    setKey(event.key);
+  };
+
+  useEffect(() => {
+    document.addEventListener('keyup', handleKeyPress);
+
+    return () => {
+      document.removeEventListener('keyup', handleKeyPress);
+    };
+  }, [key]);
+
+  return (
+    <div className="App">
+      <p className="App__message">
+        {key
+          ? `The last pressed key is [${key}]`
+          : 'Nothing was pressed yet'}
+      </p>
+    </div>
+  );
+};
