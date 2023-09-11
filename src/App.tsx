5a371f696bab358096debc25767990ac4a93ec5c
Andrii Marusiak <advo737@gmail.com>
2023-09-11 19:37:40 +0300

    add event listener, and life cycle methods

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,42 @@
 import React from 'react';
 
-export const App: React.FC = () => (
-  <div className="App">
-    <p className="App__message">The last pressed key is [Enter]</p>
-  </div>
-);
+type State = {
+  pressedKey: string;
+};
+
+export class App extends React.Component<{}, State> {
+  state: State = {
+    pressedKey: '',
+  };
+
+  componentDidMount() {
+    document.addEventListener('keyup', this.handleKeyDown);
+  }
+
+  componentWillUnmount() {
+    document.removeEventListener('keyup', this.handleKeyDown);
+  }
+
+  handleKeyDown = (event: KeyboardEvent) => {
+    this.setState(prevState => {
+      return {
+        ...prevState,
+        pressedKey: event.key,
+      };
+    });
+  };
+
+  render() {
+    const { pressedKey } = this.state;
+
+    return (
+      <div className="App">
+        <p className="App__message">
+          {pressedKey
+            ? (`The last pressed key is [${pressedKey}]`)
+            : ('Nothing was pressed yet')}
+        </p>
+      </div>
+    );
+  }
+}
