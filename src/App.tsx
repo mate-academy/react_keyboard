185dcbee912719af8240227623ac83323d4cb06a
Denys Poltorak <itwfrontend@gmail.com>
2023-09-11 16:24:06 +0300

    feat: add keyup message with class component

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,33 @@
 import React from 'react';
 
-export const App: React.FC = () => (
-  <div className="App">
-    <p className="App__message">The last pressed key is [Enter]</p>
-  </div>
-);
+export class App extends React.PureComponent<{}, { pressedKey: string }> {
+  state = {
+    pressedKey: '',
+  };
+
+  componentDidMount(): void {
+    document.addEventListener('keyup', this.handleKeyupEvent);
+  }
+
+  componentWillUnmount(): void {
+    document.removeEventListener('keyup', this.handleKeyupEvent);
+  }
+
+  handleKeyupEvent = (event: KeyboardEvent) => {
+    this.setState({ pressedKey: event.key });
+  };
+
+  render(): React.ReactNode {
+    return (
+      <div className="App">
+        <p className="App__message">
+          {this.state.pressedKey ? (
+            `The last pressed key is [${this.state.pressedKey}]`
+          ) : (
+            'Nothing was pressed yet'
+          )}
+        </p>
+      </div>
+    );
+  }
+}
