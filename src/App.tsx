def9660f92fe0507727025db669988c9cfc81f30
neovendano <kolesnyk.vik@gmail.com>
2023-09-12 20:00:43 +0300

    created class component, and it was funny

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,35 @@
 import React from 'react';
 
-export const App: React.FC = () => (
-  <div className="App">
-    <p className="App__message">The last pressed key is [Enter]</p>
-  </div>
-);
+interface State {
+  pressedKey: string,
+}
+
+export class App extends React.Component {
+  state: State = {
+    pressedKey: '',
+  };
+
+  componentDidMount(): void {
+    document.addEventListener('keyup', this.handlePressedKey);
+  }
+
+  componentWillUnmount(): void {
+    document.removeEventListener('keyup', this.handlePressedKey);
+  }
+
+  handlePressedKey = (event: KeyboardEvent) => {
+    this.setState({ pressedKey: event.key });
+  };
+
+  render() {
+    return (
+      <div className="App">
+        <p className="App__message">
+          {this.state.pressedKey.length
+            ? `The last pressed key is ${this.state.pressedKey}`
+            : 'Nothing was pressed yet'}
+        </p>
+      </div>
+    );
+  }
+}
