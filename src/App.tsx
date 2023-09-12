97cc1db2995dfef2d3c9173f260383ce561b8ddf
Sergey <serg9205@gmail.com>
2023-09-12 12:21:42 +0300

    implement keyboard logic using life methods in class component

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,44 @@
 import React from 'react';
 
-export const App: React.FC = () => (
-  <div className="App">
-    <p className="App__message">The last pressed key is [Enter]</p>
-  </div>
-);
+type Props = {};
+type State = {
+  text: string;
+};
+
+export class App extends React.Component<Props, State> {
+  state = {
+    text: '',
+  };
+
+  componentDidMount(): void {
+    this.handleState();
+  }
+
+  componentDidUpdate(): void {
+    document.addEventListener('keyup', this.handleKeyUp);
+  }
+
+  componentWillUnmount(): void {
+    document.removeEventListener('keyup', this.handleKeyUp);
+  }
+
+  handleState = () => {
+    this.setState({
+      text: 'Nothing was pressed yet',
+    });
+  };
+
+  handleKeyUp = (event: KeyboardEvent) => {
+    this.setState({
+      text: `The last pressed key is [${event.key}]`,
+    });
+  };
+
+  render() {
+    return (
+      <div className="App">
+        <p className="App__message">{this.state.text}</p>
+      </div>
+    );
+  }
+}
