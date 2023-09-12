1015c3229b4fa0cd048300a99e870a0fc09a7706
Dmytro Hrynchyi <desevoker@gmail.com>
2023-09-12 16:53:40 +0300

    add initial solution

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,40 @@
-import React from 'react';
+import { PureComponent } from 'react';
 
-export const App: React.FC = () => (
-  <div className="App">
-    <p className="App__message">The last pressed key is [Enter]</p>
-  </div>
-);
+type Props = {};
+
+type State = {
+  pressedKey: string;
+};
+
+// eslint-disable-next-line react/prefer-stateless-function
+export class App extends PureComponent<Props, State> {
+  state: State = { pressedKey: '' };
+
+  componentDidMount() {
+    document.addEventListener('keyup', this.handleKeyup);
+  }
+
+  componentWillUnmount() {
+    document.removeEventListener('keyup', this.handleKeyup);
+  }
+
+  handleKeyup = (event: KeyboardEvent) => {
+    this.setState({ pressedKey: event.key });
+  };
+
+  render() {
+    const { pressedKey } = this.state;
+
+    return (
+      <div className="App">
+        <p className="App__message">
+          {pressedKey ? (
+            `The last pressed key is [${pressedKey}]`
+          ) : (
+            'Nothing was pressed yet'
+          )}
+        </p>
+      </div>
+    );
+  }
+}
