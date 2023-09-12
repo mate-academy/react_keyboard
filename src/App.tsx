aa4db1a300abf279fd9976b1b8a55befce364b90
Diana Honcharova <fancypeanutbutter@gmail.com>
2023-09-12 12:28:15 +0300

    added App class component

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,44 @@
 import React from 'react';
+import 'bulma/css/bulma.min.css';
 
-export const App: React.FC = () => (
-  <div className="App">
-    <p className="App__message">The last pressed key is [Enter]</p>
-  </div>
-);
+type State = {
+  pressedKey: string
+};
+
+export class App extends React.Component<{}, State> {
+  state: State = {
+    pressedKey: '',
+  };
+
+  componentDidMount(): void {
+    document.addEventListener('keyup', this.handleKeyUp);
+  }
+
+  componentWillUnmount(): void {
+    document.removeEventListener('keyup', this.handleKeyUp);
+  }
+
+  handleKeyUp = (event: KeyboardEvent): void => {
+    this.setState({ pressedKey: event.key });
+  };
+
+  render() {
+    const { pressedKey } = this.state;
+
+    return (
+      <section className="hero is-fullheight">
+        <div className="hero-body has-text-centered">
+          <div className="container">
+            <div className=" box notification is-link">
+              <p>
+                {pressedKey
+                  ? (`The last pressed key is [${pressedKey}]`)
+                  : ('Nothing was pressed yet')}
+              </p>
+            </div>
+          </div>
+        </div>
+      </section>
+    );
+  }
+}
