b2e3f68d8075934d860e40c411a1969fe34d3869
Tamara Chernetska <cty2802@gmail.com>
2023-09-12 11:29:55 +0300

    removed AppMessage

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,35 @@
 import React from 'react';
-import { AppMessage } from './component/AppMessage';
 
-export const App: React.FC = () => (
-  <div className="App">
-    <AppMessage />
-  </div>
-);
+type State = {
+  pressedKey: string,
+};
+
+export class App extends React.Component<{}, State> {
+  state: State = {
+    pressedKey: '',
+  };
+
+  componentDidMount(): void {
+    document.addEventListener('keyup', this.onKeyBoard);
+  }
+
+  componentWillUnmount(): void {
+    document.removeEventListener('keyup', this.onKeyBoard);
+  }
+
+  onKeyBoard = (event: KeyboardEvent) => {
+    this.setState({ pressedKey: event.key });
+  };
+
+  render() {
+    const { pressedKey } = this.state;
+
+    return (
+      <p className="App__message">
+        {pressedKey
+          ? `The last pressed key is [${pressedKey}]`
+          : 'Nothing was pressed yet'}
+      </p>
+    );
+  }
+}
