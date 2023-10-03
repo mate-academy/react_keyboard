219911c90db1a880e383eb431d434cea9a8fcb34
Oleksandr Chernikov <pjatochkin2013@gmail.com>
2023-10-03 12:24:59 +0300

    commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,17 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 
-export const App: React.FC = () => (
-  <div className="App">
-    <p className="App__message">The last pressed key is [Enter]</p>
-  </div>
-);
+export const App: React.FC = () => {
+  const [lastPressedKey, setLastPressedKey] = useState<string>('');
+
+  useEffect(() => {
+    return document.addEventListener('keyup', (event: KeyboardEvent) => {
+      setLastPressedKey(event.key);
+    });
+  });
+
+  return (
+    <div className="App">
+      <p className="App__message">{lastPressedKey ? `The last pressed key is [${lastPressedKey}]` : 'Nothing was pressed yet'}</p>
+    </div>
+  );
+};
