626dadbf93f53a6e8d5f142ff17e3bfdefb97a50
Denys <kolbasin1990@gmail.com>
2023-10-05 12:34:29 +0300

    react_keyboard_v-01

--- src/App.tsx
+++ src/App.tsx
@@ -2,17 +2,12 @@ import React from 'react';
 
 type State = {
   pressedKey: string;
-}
+};
 
 export class App extends React.Component<State> {
   state: State = {
     pressedKey: 'Nothing was pressed yet',
-  }
-
-  keyUpHandler = (event: KeyboardEvent) => {
-    this.setState({ pressedKey: `The last pressed key is [${event.key}]` });
-    console.log(event.key);
-  }
+  };
 
   componentDidMount(): void {
     document.addEventListener('keyup', this.keyUpHandler);
@@ -22,12 +17,15 @@ export class App extends React.Component<State> {
     document.removeEventListener('keyup', this.keyUpHandler);
   }
 
+  keyUpHandler = (event: KeyboardEvent) => {
+    this.setState({ pressedKey: `The last pressed key is [${event.key}]` });
+  };
+
   render() {
     return (
       <div className="App">
         <p className="App__message">{this.state.pressedKey}</p>
       </div>
-    )
+    );
   }
 }
-
