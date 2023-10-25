0e8eb65fc72178b5deec2429344a1e9e6084a194
Alex <opetrovskyi423@gmail.com>
2023-10-25 15:14:54 +0200

    fix

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,37 @@
 import React from 'react';
-import Message from './components/Message';
 
-export const App: React.FC = () => (
-  <div className="App">
-    <Message />
-  </div>
-);
+type State = {
+  currentKey: string | null;
+};
+
+export class App extends React.Component {
+  state: State = {
+    currentKey: null,
+  };
+
+  componentDidMount() {
+    document.addEventListener('keyup', (event: KeyboardEvent) => {
+      this.setState({ currentKey: event.key });
+    });
+  }
+
+  componentWillUnmount() {
+    document.removeEventListener('keyup', (event: KeyboardEvent) => {
+      this.setState({ currentKey: event.key });
+    });
+  }
+
+  render() {
+    const { currentKey } = this.state;
+
+    return (
+      <div className="App">
+        <p className="App__message">
+          {currentKey
+            ? `The last pressed key is[${currentKey}]`
+            : 'Nothing was pressed yet'}
+        </p>
+      </div>
+    );
+  }
+}
