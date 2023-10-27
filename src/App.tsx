a5659fed49fcbb7d5782045de2f054fb4b3d8909
OlegRak86 <rakolegit@gmail.com>
2023-10-27 19:33:05 +0300

    react_keyboard

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,33 @@
 import React from 'react';
 
-export const App: React.FC = () => (
-  <div className="App">
-    <p className="App__message">The last pressed key is [Enter]</p>
-  </div>
-);
+type State = {
+  keys :string
+};
+// eslint-disable-next-line react/prefer-stateless-function
+export class App extends React.Component<State> {
+  state:State = {
+    keys: 'Nothing was pressed yet',
+  };
+
+  componentDidMount(): void {
+    document.addEventListener('keyup', this.handlebooton);
+  }
+
+  componentWillUnmount():void {
+    document.removeEventListener('keyup', this.handlebooton);
+  }
+
+  handlebooton = (event: KeyboardEvent) => {
+    this.setState({ keys: `The last pressed key is [${event.key}]` });
+  };
+
+  render() {
+    const { keys } = this.state;
+
+    return (
+      <div className="App">
+        <p className="App__message">{keys}</p>
+      </div>
+    );
+  }
+}
