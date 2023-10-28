1c2ceaf337b5a0f425d445c7ab1d311d5fb47ee0
elenagrasovskaya <elenagrasovskaya@gmail.com>
2023-10-28 23:37:42 +0300

    Keyboard tracker class component

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,69 @@
-import React from 'react';
+import React from "react";
 
-export const App: React.FC = () => (
-  <div className="App">
-    <p className="App__message">The last pressed key is [Enter]</p>
-  </div>
-);
+interface State {
+  char: string;
+}
+
+export class App extends React.Component {
+  state: Readonly<State>;
+
+  constructor(props: any) {
+    super(props);
+    this.state = { char: "" };
+  }
+
+  onKeyboardEvent = (event: KeyboardEvent) => {
+    this.setState({char:event.key});
+  };
+
+  componentDidMount(): void {
+    document.addEventListener("keyup", this.onKeyboardEvent);
+  }
+
+  componentWillUnmount(): void {
+    document.removeEventListener("keyup", this.onKeyboardEvent);
+  }
+
+  render() {
+    return (
+      <div className="App">
+        {this.state.char ? (
+          <p className="App__message">{`The last pressed key is [${this.state.char}]`}</p>
+        ) : (
+          <p className="App__message">Nothing was pressed yet</p>
+        )}
+      </div>
+    );
+  }
+}
+
+/*
+REACT FUNCTIONAL COMPONENT
+
+import React, { useState, useEffect, useCallback } from "react";
+ export const App: React.FC = () => {
+  const [char, setChar] = useState("");
+
+  const checkKey = useCallback((event: KeyboardEvent) => {
+    console.log(event.key);
+    setChar(event.key);
+  }, []);
+
+  useEffect(() => {
+    document.addEventListener("keyup", checkKey);
+
+    return () => {
+      document.removeEventListener("keyup", checkKey);
+    };
+  }, []);
+
+  return (
+    <div className="App">
+      {char ? (
+        <p className="App__message">{`The last pressed key is [${char}]`}</p>
+      ) : (
+        <p className="App__message">Nothing was pressed yet</p>
+      )}
+    </div>
+  );
+}; */
