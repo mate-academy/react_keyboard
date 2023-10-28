e998f99c219163eaaeb14d972ec7fa06cda6c744
Artem Trofymovych <navrotskymetra@outlook.com>
2023-10-28 10:41:24 +0200

    solution fix

--- src/index.tsx
+++ src/index.tsx
@@ -1,14 +1,9 @@
 import ReactDOM from 'react-dom';
-import React from 'react';
 import { App } from './components/App/App';
-import { App1 } from './components/App1/App1';
 
 import './index.scss';
 
 ReactDOM.render(
-  <div>
-    <App />
-    <App1 />
-  </div>,
+  <App />,
   document.getElementById('root'),
 );
