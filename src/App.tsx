2cb8f4fccc2f2569702f0377896ef64f035ed7a6
aurelia <aurelia.gorzna.dev@gmail.com>
2023-12-06 13:38:29 +0100

    solution

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,35 @@
+/* eslint-disable react/jsx-one-expression-per-line */
 import React from 'react';
 
-export const App: React.FC = () => (
-  <div className="App">
-    <p className="App__message">The last pressed key is [Enter]</p>
-  </div>
-);
+interface State {
+  message: string
+  lastClickedKey: string
+}
+
+export class App extends React.Component {
+  state: State = {
+    message: 'Nothing was pressed yet',
+    lastClickedKey: '',
+  };
+
+  componentDidMount() {
+    window.addEventListener('keydown', (e: KeyboardEvent) => {
+      this.setState({
+        message: 'The last pressed key is',
+        lastClickedKey: e.key,
+      });
+    });
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <p className="App__message">
+          {this.state.lastClickedKey === ''
+            ? this.state.message
+            : `${this.state.message} [${this.state.lastClickedKey}]`}
+        </p>
+      </div>
+    );
+  }
+}
