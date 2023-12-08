c4a09d9af043bc0e9cbf04b52671d90114e5f45e
Dmytro Yefimov <red.eye77seven@gmail.com>
2023-12-08 12:28:24 +0200

    finish task

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,37 @@
-import React from 'react';
+import React, { PureComponent } from 'react';
 
-export const App: React.FC = () => (
-  <div className="App">
-    <p className="App__message">The last pressed key is [Enter]</p>
-  </div>
-);
+export class App extends PureComponent {
+  state = {
+    key: '',
+  };
+
+  componentDidMount(): void {
+    document.addEventListener('keyup', this.onKeyPress);
+  }
+
+  componentWillUnmount(): void {
+    document.removeEventListener('keyup', this.onKeyPress);
+  }
+
+  onKeyPress = (event: KeyboardEvent): void => {
+    this.setState({ key: event.key });
+  };
+
+  render(): React.ReactNode {
+    const { key } = this.state;
+
+    return (
+      <>
+        <div className="App">
+          <p
+            className="App__message"
+          >
+            {key
+              ? `The last pressed key is ${key}`
+              : 'Nothing was pressed yet'}
+          </p>
+        </div>
+      </>
+    );
+  }
+}
