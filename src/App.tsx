c01ae584a60bdc7c36b9d7078324c65d93db6ebf
Oleksandr Robenko <oleksandr.robenko@gmail.com>
2023-12-20 16:59:48 +0200

    add solution v1.0 with KeyboardEvent

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,25 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 
-export const App: React.FC = () => (
-  <div className="App">
-    <p className="App__message">The last pressed key is [Enter]</p>
-  </div>
-);
+export const App: React.FC = () => {
+  const [pressedKey, setPressedKey] = React.useState<string>('');
+
+  useEffect(() => {
+    const handleKeyDown = (event: KeyboardEvent) => {
+      setPressedKey(event.key);
+    };
+
+    window.addEventListener('keydown', handleKeyDown);
+
+    return () => {
+      window.removeEventListener('keydown', handleKeyDown);
+    };
+  }, []);
+
+  return (
+    <div className="App">
+      <p className="App__message">
+        {pressedKey ? `Pressed key: ${pressedKey}` : 'Nothing was pressed yet'}
+      </p>
+    </div>
+  );
+};
