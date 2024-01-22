f5b54d8a0b8a19c87df017cf033f0c62cb9cd4c3
Oleksandr Boiko <iccc97@gmail.com>
2024-01-22 14:59:15 +0200

    add key class-component Keyboard

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,6 @@
 import React from 'react';
+import { Keyboard } from './components/Keyboard';
 
 export const App: React.FC = () => (
-  <div className="App">
-    <p className="App__message">The last pressed key is [Enter]</p>
-  </div>
+  <Keyboard />
 );
