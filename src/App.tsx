d8430237f8bc5804b2f4136904000e6e5f27110a
Olena Shapkun <shapkunkh@gmail.com>
2024-01-30 20:19:32 +0200

    correction

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,5 @@
 import React from 'react';
 
-// export const App: React.FC = () => (
-//   <div className="App">
-//     <p className="App__message">The last pressed key is [Enter]</p>
-//   </div>
-// );
 type State = {
   message: string;
 };
@@ -16,18 +11,17 @@ export class App extends React.PureComponent<State> {
 
   componentDidMount(): void {
     document.addEventListener('keyup', this.handleMessage);
-  };
+  }
 
   componentWillUnmount(): void {
     document.removeEventListener('keyup', this.handleMessage);
-  };
+  }
 
-   handleMessage = (event: KeyboardEvent) => {
+  handleMessage = (event: KeyboardEvent) => {
     this.setState({
       message: `The last pressed key is [${event.key}]`,
     });
   };
-
 
   render() {
     const { message } = this.state;
