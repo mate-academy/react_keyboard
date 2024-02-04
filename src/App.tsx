191e020e6bb8f123fc5add0ea4fbfa9263a3d661
Nazar Pastuschak <kns19pna@gmail.com>
2024-02-04 14:45:08 +0200

    add task keyboard

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,39 @@
 import React from 'react';
 
-export const App: React.FC = () => (
-  <div className="App">
-    <p className="App__message">The last pressed key is [Enter]</p>
-  </div>
-);
+interface State {
+  pressedKey: string;
+}
+// eslint-disable-next-line react/prefer-stateless-function
+export class App extends React.Component<{}, State> {
+  constructor(props: {}) {
+    super(props);
+    // eslint-disable-next-line react/state-in-constructor
+    this.state = {
+      pressedKey: '',
+    };
+  }
+
+  componentDidMount(): void {
+    document.addEventListener('keyup', this.handleKeyUp);
+  }
+
+  componentWillUnmount(): void {
+    document.removeEventListener('keyup', this.handleKeyUp);
+  }
+
+  handleKeyUp = (event: KeyboardEvent): void => {
+    this.setState({ pressedKey: event.key });
+  };
+
+  render() {
+    const { pressedKey } = this.state;
+
+    return (
+      <div className="app">
+        <p className="App__message">
+          {pressedKey !== '' ? `The last pressed key is [${pressedKey}]` : 'Nothing was pressed yet'}
+        </p>
+      </div>
+    );
+  }
+}
