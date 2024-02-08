a0a6c2cbc10ce8e6227de2d66ffbb0218781bc99
Krzychu92 <138024608+Krzychu92@users.noreply.github.com>
2024-02-08 21:08:52 +0100

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -20,7 +20,7 @@ export class App extends React.Component {
       <div className="App">
         <p className="App__message">
           {this.state.pressedKey
-            ? `The last pressed key is ${this.state.pressedKey}`
+            ? `The last pressed key is [${this.state.pressedKey}]`
             : 'Nothing was pressed yet'}
         </p>
       </div>
