627d794b2bce819e44db1847c1506e5f514ce9a2
spencerspe <spencerspe@gmail.com>
2024-02-09 15:30:17 +0000

    first verion

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,41 @@
 import React from 'react';
 
-export const App: React.FC = () => (
-  <div className="App">
-    <p className="App__message">The last pressed key is [Enter]</p>
-  </div>
-);
+type Props = {
+  letter: string;
+};
+
+type State = {
+  keyHit: string | null;
+  message: string;
+};
+
+export class App extends React.Component<Props, State> {
+  state = {
+    keyHit: null,
+    message: 'Nothing was pressed yet',
+  };
+
+  componentDidMount() {
+    document.addEventListener('keydown', (event: KeyboardEvent) => {
+      this.setState({ keyHit: event.key });
+      this.setState({ message: `The last pressed key is [${event.key}]` });
+    });
+  }
+
+  componentWillUnmount() {
+    document.removeEventListener('keydown', (event: KeyboardEvent) => {
+      this.setState({ keyHit: event.key });
+    });
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <p className="App__message">
+          { this.state.keyHit ? this.state.message : 'Nothing was pressed yet' }
+
+        </p>
+      </div>
+    );
+  }
+}
