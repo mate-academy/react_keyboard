36b932ed9d56b1c9a87a4d54a7711fb9bcd1a557
[JokerMTM] <martunmtm@gmail.com>
2024-02-14 21:10:12 +0200

    add_keyboard

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,37 @@
-import React from 'react';
+import { Component } from 'react';
 
-export const App: React.FC = () => (
-  <div className="App">
-    <p className="App__message">The last pressed key is [Enter]</p>
-  </div>
-);
+type State = {
+  key: string;
+};
+
+export class App extends Component {
+  state: Readonly<State> = {
+    key: '',
+  };
+
+  componentDidMount() {
+    window.addEventListener('keyup', this.handleKeyPress);
+  }
+
+  componentWillUnmount() {
+    window.removeEventListener('keyup', this.handleKeyPress);
+  }
+
+  handleKeyPress = (event: KeyboardEvent) => {
+    this.setState({ key: event.key });
+  };
+
+  render() {
+    let message = 'Nothing was pressed yet';
+
+    if (this.state.key !== '') {
+      message = `The last pressed key is [${this.state.key}]`;
+    }
+
+    return (
+      <div className="App">
+        <p className="App__message">{message}</p>
+      </div>
+    );
+  }
+}
