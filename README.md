18d4f88bc2d5a9a9686076cad34eaef26443ca9c
Artem Vlasiuk <artemvlasiuk@93gmail.com>
2024-02-17 04:42:51 +0200

    solution

--- README.md
+++ README.md
@@ -1,3 +1,4 @@
+[DEMO LINK](https://artemvlasiuk.github.io/react_keyboard/)
 # React Keyboard
 
 > [React + Typescript cheat sheet](https://mate-academy.github.io/fe-program/js/extra/react-typescript)
@@ -22,4 +23,4 @@ Make the `App` a class component with `pressedKey` in the `state`.
 - Implement a solution following the [React task guideline](https://github.com/mate-academy/react_task-guideline#react-tasks-guideline).
 - Use the [React TypeScript cheat sheet](https://mate-academy.github.io/fe-program/js/extra/react-typescript).
 - Open one more terminal and run tests with `npm test` to ensure your solution is correct.
-- Replace `<your_account>` with your Github username in the [DEMO LINK](https://<your_account>.github.io/react_keyboard/) and add it to the PR description.
+- Replace `<your_account>` with your Github username in the [DEMO LINK](https://artemvlasiuk.github.io/react_keyboard/) and add it to the PR description.
--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,37 @@
 import React from 'react';
 
-export const App: React.FC = () => (
-  <div className="App">
-    <p className="App__message">The last pressed key is [Enter]</p>
-  </div>
-);
+/* type State = {
+  pressedKey: string;
+}; */
+
+export class App extends React.Component {
+  state = {
+    pressedKey: 'Nothing was pressed yet',
+  };
+
+  componentDidMount() {
+    window.addEventListener('keyup', this.handleKeyDown);
+  }
+
+  componentWillUnmount() {
+    document.removeEventListener('keyup', this.handleKeyDown);
+  }
+
+  handleKeyDown = (event: KeyboardEvent) => {
+    this.setState({ pressedKey: event.key });
+  };
+
+  render() {
+    const { pressedKey } = this.state;
+
+    return (
+      <div className="App">
+        <p className="App__message">
+          {pressedKey === 'Nothing was pressed yet'
+            ? pressedKey
+            : `The last pressed key is [${pressedKey}]`}
+        </p>
+      </div>
+    );
+  }
+}
