df7b4ca3abe05668675b4ac4e56fe853c60e9887
Illya Katynskyi <katinskyi.illia@lll.kpi.ua>
2024-02-18 09:35:00 +0200

    Change index.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -24,11 +24,11 @@ export class App extends React.Component<{}, State> {
   render() {
     return (
       <div className="App">
-        {this.state.pressedKey ? (
-          <p className="App__message">{`The last pressed key is [${this.state.pressedKey}]`}</p>
-        ) : (
-          <p className="App__message">Nothing was pressed yet</p>
-        )}
+        <p className="App__message">
+          {this.state.pressedKey
+            ? `The last pressed key is [${this.state.pressedKey}]`
+            : 'Nothing was pressed yet'}
+        </p>
       </div>
     );
   }
