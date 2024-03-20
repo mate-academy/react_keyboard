0d2b14bb415b2fe321312cc7b1e9c55a4860304c
Denis Kotov <dionisiy2904@gmail.com>
2024-03-20 16:33:08 +0200

    keyboardfix

--- src/index.tsx
+++ src/index.tsx
@@ -2,6 +2,5 @@ import { createRoot } from 'react-dom/client';
 import { App } from './App';
 
 import './index.scss';
-import React from 'react';
 
 createRoot(document.getElementById('root') as HTMLElement).render(<App />);
