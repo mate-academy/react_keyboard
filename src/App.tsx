b90191a5523e20e5ac0cb9388dcaa34f9eec5921
DESKTOP-EHDJ2FA\yarik <yarik.zubyk@gmail.com>
2024-04-02 17:42:26 +0300

    added lifestyle component

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,39 @@
 import React from 'react';
 
-export const App: React.FC = () => (
-  <div className="App">
-    <p className="App__message">The last pressed key is [Enter]</p>
-  </div>
-);
+type State = {
+  keyUp: string;
+};
+
+export class App extends React.Component<{}, State> {
+  state: State = {
+    keyUp: '',
+  };
+
+  handlerClickKeyButton = (event: KeyboardEvent) => {
+    this.setState({
+      keyUp: event.key,
+    });
+  };
+
+  componentDidMount() {
+    document.addEventListener('keyup', this.handlerClickKeyButton);
+  }
+
+  shouldComponentUpdate(nextState: Readonly<State>): boolean {
+    return nextState.keyUp !== this.state.keyUp;
+  }
+
+  componentWillUnmount(): void {
+    document.removeEventListener('keyup', this.handlerClickKeyButton);
+  }
+
+  render(): React.ReactNode {
+    return this.state.keyUp ? (
+      <p className="App__message">
+        The last pressed key is {`[${this.state.keyUp}]`}
+      </p>
+    ) : (
+      <p className="App__message">Nothing was pressed yet</p>
+    );
+  }
+}
