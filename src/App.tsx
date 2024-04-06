48b9460ea4f4bb5d5249093df5c29cbba38b7837
Oleh Marushchak <westernpc22@gmail.com>
2024-04-06 13:48:56 +0300

    add test

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,29 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 
-export const App: React.FC = () => (
-  <div className="App">
-    <p className="App__message">The last pressed key is [Enter]</p>
-  </div>
-);
+export const App: React.FC = () => {
+  const [key, setKey] = useState('');
+
+  useEffect(() => {
+    const handleKeyClick = (event: KeyboardEvent) => {
+      setKey(event.key);
+    }; // event
+
+    document.addEventListener('keyup', handleKeyClick);
+    // componentDidMount (turn on listenr when component on first render)
+
+    return () => {
+      document.removeEventListener('keyup', handleKeyClick);
+    };
+    // componentWillUnmount delete this Listener when component App. hide
+  }, []);
+
+  const hasKey = key
+    ? `The last pressed key is [${key}]`
+    : 'Nothing was pressed yet';
+
+  return (
+    <div className="App">
+      <p className="App__message">{hasKey}</p>
+    </div>
+  );
+};
