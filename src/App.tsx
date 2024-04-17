f4a771d9b72dd3239914b5af9d2456afdeddd9e2
KatsuboDmytro <dmitriyofficial03@gmaik.com>
2024-04-17 16:43:26 +0300

    autocomplete

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,32 @@
 import React from 'react';
 
-export const App: React.FC = () => (
-  <div className="App">
-    <p className="App__message">The last pressed key is [Enter]</p>
-  </div>
-);
+export class App extends React.Component {
+  state = {
+    pressedKey: null,
+  };
+
+  componentDidMount() {
+    document.addEventListener('keyup', this.handleKeyUp);
+  }
+
+  componentWillUnmount() {
+    document.removeEventListener('keyup', this.handleKeyUp);
+  }
+
+  handleKeyUp = (event: KeyboardEvent): void => {
+    const { key } = event;
+
+    this.setState({ pressedKey: key });
+  };
+
+  render() {
+    const { pressedKey } = this.state;
+    const message = pressedKey ? `The last pressed key is [${pressedKey}]` : 'Nothing was pressed yet';
+
+    return (
+      <div className="App">
+        <p className="App__message">{message}</p>
+      </div>
+    );
+  }
+}
