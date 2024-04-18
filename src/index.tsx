79f1c76ce25bad2318552906c487145906b0616e
Andrey <galosaandrew@gamil.com>
2024-04-18 11:30:47 +0300

    Solution

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,9 @@
 import { createRoot } from 'react-dom/client';
+import './index.scss';
 import { App } from './App';
 
-import './index.scss';
-
-createRoot(document.getElementById('root') as HTMLElement).render(<App />);
+createRoot(document.getElementById('root') as HTMLElement).render(
+  <>
+    <App />
+  </>,
+);
