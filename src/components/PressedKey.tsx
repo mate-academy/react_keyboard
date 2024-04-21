490aa088256c08c7abdcd886183d8b8844d4ae1a
Oleksandr <freeromanov@gmail.com>
2024-04-21 22:44:38 +0300

    add task solution

---
+++ src/components/PressedKey.tsx
@@ -0,0 +1,37 @@
+import React from 'react';
+
+type AppState = {
+  pressedKey: string | null;
+};
+
+export class PressedKey extends React.Component<{}, AppState> {
+  state: AppState = {
+    pressedKey: null,
+  };
+
+  componentDidMount() {
+    document.addEventListener('keyup', this.handleKeyUp);
+  }
+
+  componentWillUnmount() {
+    document.removeEventListener('keyup', this.handleKeyUp);
+  }
+
+  handleKeyUp = (event: KeyboardEvent) => {
+    this.setState({ pressedKey: event.key });
+  };
+
+  render() {
+    const { pressedKey } = this.state;
+
+    return (
+      <div className="App">
+        {pressedKey ? (
+          <p className="App__message">The last pressed key is [{pressedKey}]</p>
+        ) : (
+          <p className="App__message">Nothing was pressed yet</p>
+        )}
+      </div>
+    );
+  }
+}
