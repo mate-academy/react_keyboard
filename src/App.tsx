b2501b42e3d89e686040644f56831ff4783e9e84
ordila <ordila99@gmail.com>
2024-05-08 21:29:00 +0300

    add solving

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,35 @@
 import React from 'react';
 
-export const App: React.FC = () => (
-  <div className="App">
-    <p className="App__message">The last pressed key is [Enter]</p>
-  </div>
-);
+type State = {
+  key: null | string;
+};
+
+export class App extends React.Component<{}, State> {
+  state: State = {
+    key: null,
+  };
+
+  onKeyboardPress = (event: KeyboardEvent) => {
+    this.setState({ key: event.key });
+  };
+
+  componentDidMount(): void {
+    document.addEventListener('keyup', this.onKeyboardPress);
+  }
+
+  componentWillUnmount(): void {
+    document.removeEventListener('keyup', this.onKeyboardPress);
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <p className="App__message">
+          {!this.state.key
+            ? 'Nothing was pressed yet'
+            : `The last pressed key is [${this.state.key}]`}
+        </p>
+      </div>
+    );
+  }
+}
