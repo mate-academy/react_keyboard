4ce2959d510e14f0cacbff97975aff88053fc699
Vladislav <7evenLays@gmail.com>
2024-05-08 17:25:17 +0300

    keybord solution

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,37 @@
 import React from 'react';
 
-export const App: React.FC = () => (
-  <div className="App">
-    <p className="App__message">The last pressed key is [Enter]</p>
-  </div>
-);
+type State = {
+  pressedKey: string;
+};
+
+export class App extends React.Component {
+  state: State = {
+    pressedKey: '',
+  };
+
+  handleDocumentKeybordClick = (e: KeyboardEvent) => {
+    this.setState({ pressedKey: e.key }, () => {});
+  };
+
+  componentDidMount(): void {
+    document.addEventListener('keyup', this.handleDocumentKeybordClick);
+    window.console.log('componentDidMount');
+  }
+
+  componentWillUnmount(): void {
+    document.removeEventListener('keyup', this.handleDocumentKeybordClick);
+    window.console.log('componentWillUnmount');
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <p className="App__message">
+          {!this.state.pressedKey
+            ? `Nothing was pressed yet`
+            : `The last pressed key is [${this.state.pressedKey}]`}
+        </p>
+      </div>
+    );
+  }
+}
