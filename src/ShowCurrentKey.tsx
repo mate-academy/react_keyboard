0b693387d4c51c15a902f797bf0f82e586ba10e3
Yarik kedrych <yarik.kedrych9@gmail.com>
2024-08-19 20:47:20 +0300

    add solution

---
+++ src/ShowCurrentKey.tsx
@@ -0,0 +1,37 @@
+import React from 'react';
+
+type State = {
+  pressedKey: string;
+};
+
+export class ShowCurrentKey extends React.Component<{}, State> {
+  state = {
+    pressedKey: '',
+  };
+
+  componentDidMount() {
+    document.addEventListener('keyup', this.handlePressedKey);
+  }
+
+  componentWillUnmount() {
+    document.removeEventListener('keyup', this.handlePressedKey);
+  }
+
+  handlePressedKey = (event: KeyboardEvent) => {
+    this.setState({ pressedKey: event.key });
+  };
+
+  render() {
+    const { pressedKey } = this.state;
+
+    return (
+      <div className="App">
+        {pressedKey ? (
+          <p className="App__message">The last pressed key is [{pressedKey}]</p>
+        ) : (
+          <p className="App__message">Nothing was pressed yet</p>
+        )}
+      </div>
+    );
+  }
+}
