718ff755f7f81ded6e9a6bff6a8c42bd9983760b
AmateZz <yarike451@gmail.com>
2024-09-13 11:56:50 +0300

    problem

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,50 @@
 import React from 'react';
 
-export const App: React.FC = () => (
-  <div className="App">
-    <p className="App__message">The last pressed key is [Enter]</p>
-  </div>
-);
+type AppState = {
+  key: string;
+  message: string;
+};
+
+class App extends React.Component<{}, AppState> {
+  // Инициализация состояния как поле класса
+  state: AppState = {
+    key: '', // Хранит нажатую клавишу
+    message: 'Nothing was pressed yet', // Начальное сообщение
+  };
+
+  // Добавление обработчика keyup при монтировании компонента
+  componentDidMount() {
+    document.addEventListener('keyup', this.handleKeyUp);
+  }
+
+  // Удаление обработчика keyup при размонтировании компонента
+  componentWillUnmount() {
+    document.removeEventListener('keyup', this.handleKeyUp);
+  }
+
+  // Обработчик события keyup, который обновляет состояние
+  handleKeyUp = (event: KeyboardEvent) => {
+    this.setState({ key: event.key }, this.updateMessage);
+  };
+
+  // Метод для обновления сообщения
+  updateMessage = () => {
+    const { key } = this.state;
+
+    if (key === '') {
+      this.setState({ message: 'Nothing was pressed yet' });
+    } else {
+      this.setState({ message: `The last pressed key is [${key}]` });
+    }
+  };
+
+  render() {
+    return (
+      <div className="App">
+        <p className="App__message">{this.state.message}</p>
+      </div>
+    );
+  }
+}
+
+export default App;
