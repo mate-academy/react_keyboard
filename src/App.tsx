3c07b878d067e2af59ee3affc50ee6ae720f1633
mikezhylka <115790814+mikezhylka@users.noreply.github.com>
2024-10-02 14:46:21 +0200

    feat(App): extend class according to solution

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,37 @@
 import React from 'react';
 
-export const App: React.FC = () => (
-  <div className="App">
-    <p className="App__message">The last pressed key is [Enter]</p>
-  </div>
-);
+type State = {
+  pressedKey: string;
+};
+
+export class App extends React.Component<{}, State> {
+  state: State = {
+    pressedKey: '',
+  };
+
+  handleKeyboardClick = (event: KeyboardEvent) => {
+    return event.key !== 'F5' && this.setState({ pressedKey: event.key });
+  };
+
+  componentDidMount(): void {
+    document.addEventListener('keyup', this.handleKeyboardClick);
+  }
+
+  componentWillUnmount(): void {
+    document.removeEventListener('keyup', this.handleKeyboardClick);
+  }
+
+  render() {
+    const isKeyPressed = this.state.pressedKey;
+
+    return (
+      <div className="App">
+        <p className="App__message">
+          {isKeyPressed
+            ? `The last pressed key is [${this.state.pressedKey}]`
+            : 'Nothing was pressed yet'}
+        </p>
+      </div>
+    );
+  }
+}
