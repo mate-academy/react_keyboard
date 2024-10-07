c2c250f52f04e5b538f54bfebea794a88bb14831
Maksym Mohyla <greenparksquad@gmail.com>
2024-10-07 15:02:00 +0300

    last pressed key is shown

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,28 @@
 import React from 'react';
+import { render } from 'react-dom';
 
-export const App: React.FC = () => (
-  <div className="App">
-    <p className="App__message">The last pressed key is [Enter]</p>
-  </div>
-);
+export class App extends React.Component {
+  state = {
+    message: 'Nothing was pressed yet',
+  };
+
+  handleKeyUp = (event: KeyboardEvent) => {
+    this.setState({ message: `The last pressed key is [${event.key}]` });
+  };
+
+  componentDidMount(): void {
+    document.addEventListener('keyup', this.handleKeyUp);
+  }
+
+  componentWillUnmount(): void {
+    document.removeEventListener('keyup', this.handleKeyUp);
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <p className="App__message">{this.state.message}</p>
+      </div>
+    );
+  }
+}
