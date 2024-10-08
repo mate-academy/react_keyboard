8e494fe897951bfd4c86d2f2bf3841bd639eb5c7
vikshiyan <miss.shiyan@gmail.com>
2024-10-09 00:22:01 +0300

    Solution

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,3 @@
-// import React from 'react';
-
-// export const App: React.FC = () => (
-//   <div className="App">
-//     <p className="App__message">The last pressed key is [Enter]</p>
-//   </div>
-// );
-
 import React from 'react';
 
 type State = {
