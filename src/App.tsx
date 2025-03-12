d49863386e1fbc91b2cbec07f0040443bc82ebaf
Volodymyr Bardyuzha <bardyuzha@gmail.com>
2025-03-11 21:21:48 -0500

    rewrote component to class based

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,35 @@
-import React, { useEffect, useState } from 'react';
+import React from 'react';
 
-export const App: React.FC = () => {
-  const [key, setKey] = useState<string | null>(null);
-
-  useEffect(() => {
-    const handleKeyUp = (event: KeyboardEvent) => {
-      setKey(event.key);
-    };
-
-    document.addEventListener('keyup', handleKeyUp);
-
-    return () => {
-      document.removeEventListener('keyup', handleKeyUp);
-    };
-  }, []);
-
-  return (
-    <div className="App">
-      <p className="App__message">
-        {key === null && 'Nothing was pressed yet'}
-        {key !== null && `The last pressed key is [${key}]`}
-      </p>
-    </div>
-  );
+type State = {
+  pressedKey: null | string;
 };
+
+export class App extends React.Component {
+  state: State = {
+    pressedKey: null,
+  };
+
+  componentDidMount(): void {
+    document.addEventListener('keyup', (event: KeyboardEvent) => {
+      this.setState({ pressedKey: event.key });
+    });
+  }
+
+  componentWillUnmount(): void {
+    document.removeEventListener('keyup', (event: KeyboardEvent) => {
+      this.setState({ pressedKey: event.key });
+    });
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <p className="App__message">
+          {this.state.pressedKey === null && 'Nothing was pressed yet'}
+          {this.state.pressedKey !== null &&
+            `The last pressed key is [${this.state.pressedKey}]`}
+        </p>
+      </div>
+    );
+  }
+}
