a8cfe066ee685462b051adebfacf0918bcc0e93a
maksimka-gif <maks.markevych@gmail.com>
2025-04-10 00:45:00 +0300

    solution

--- src/App.tsx
+++ src/App.tsx
@@ -9,16 +9,16 @@ export class App extends React.Component<{}, State> {
     pressedKey: '',
   };
 
-  onKeyUp = (event: KeyboardEvent) => {
+  handleKeyUp = (event: KeyboardEvent) => {
     this.setState({ pressedKey: event.key });
   };
 
   componentDidMount(): void {
-    window.addEventListener('keyup', this.onKeyUp);
+    window.addEventListener('keyup', this.handleKeyUp);
   }
 
   componentWillUnmount(): void {
-    window.removeEventListener('keyup', this.onKeyUp);
+    window.removeEventListener('keyup', this.handleKeyUp);
   }
 
   render() {
@@ -26,9 +26,11 @@ export class App extends React.Component<{}, State> {
 
     return (
       <div className="App">
-        {pressedKey
-          ? <p className="App__message">The last pressed key is [{pressedKey}]</p>
-          : <p className="App__message">Nothing was pressed yet</p>}
+        {pressedKey ? (
+          <p className="App__message">The last pressed key is [{pressedKey}]</p>
+        ) : (
+          <p className="App__message">Nothing was pressed yet</p>
+        )}
       </div>
     );
   }
