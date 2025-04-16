ffc02f6691c6f5a5615fcc81a8891d8daad12b12
Ivan Ryabukha <ryabukhaivan98@gmail.com>
2025-04-16 15:58:51 +0300

    fix2 task solution

--- src/App.tsx
+++ src/App.tsx
@@ -1,27 +1,21 @@
 import React from 'react';
 
-// export const App: React.FC = () => (
-//   <div className="App">
-//     <p className="App__message">The last pressed key is [Enter]</p>
-//   </div>
-// );
-
 type Props = {
   key: string;
 };
 
 type State = {
-  pressKey: string;
+  pressedKey: string;
 };
 
 export class App extends React.Component<Props, State> {
   state: State = {
-    pressKey: '',
+    pressedKey: '',
   };
 
   handleDocumentKeyUp = (event: KeyboardEvent) => {
     event.preventDefault();
-    this.setState({ pressKey: event.key });
+    this.setState({ pressedKey: event.key });
   };
 
   componentDidMount(): void {
@@ -33,12 +27,14 @@ export class App extends React.Component<Props, State> {
   }
 
   render(): React.ReactNode {
+    const key = this.state.pressedKey;
+
     return (
       <div className="App">
         <p className="App__message">
           {`${
-            this.state.pressKey
-              ? `${`The last pressed key is [${this.state.pressKey}]`}`
+            key
+              ? `${`The last pressed key is [${key}]`}`
               : 'Nothing was pressed yet'
           }`}
         </p>
