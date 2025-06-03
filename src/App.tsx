6378bef0f438f7f38716bd7ff4de8bd814d70a0b
Gregory Korsun <lvlup9119@gmail.com>
2025-06-03 13:53:28 +0300

    Add task solution

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,23 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 
-export const App: React.FC = () => (
-  <div className="App">
-    <p className="App__message">The last pressed key is [Enter]</p>
-  </div>
-);
+export const App: React.FC = () => {
+  const [symbol, setSybol] = useState<string | null>(null);
+
+  useEffect(() => {
+    const handleKeyPress = (event: KeyboardEvent) => setSybol(event.key);
+
+    document.addEventListener('keyup', handleKeyPress);
+
+    return () => document.removeEventListener('keyup', handleKeyPress);
+  }, [symbol]);
+
+  return (
+    <div className="App">
+      <p className="App__message">
+        {symbol === null
+          ? 'Nothing was pressed yet'
+          : `The last pressed key is [${symbol}]`}
+      </p>
+    </div>
+  );
+};
