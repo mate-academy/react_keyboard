8043bd618296944cb9e8ca8ad4e521074e257e63
Paul Kotsan <paul@Pavlos-MacBook-Pro.local>
2025-07-23 14:20:55 -0400

    Solution

---
+++ src/ClassComponents/KeyboardInput.tsx
@@ -0,0 +1,38 @@
+import React, { KeyboardEvent } from 'react';
+
+type State = {
+  keyPressed: string;
+};
+
+export class KeyboardInput extends React.Component {
+  state: State = {
+    keyPressed: '',
+  };
+
+  componentDidMount(): void {
+    window.addEventListener('keyup', this.handleKeyChange);
+  }
+
+  componentWillUnmount(): void {
+    window.removeEventListener('keyup', this.handleKeyChange);
+  }
+
+  handleKeyChange = (event: React.KeyboardEvent) => {
+    event.preventDefault();
+    this.setState({ keyPressed: event.key });
+  };
+
+  render() {
+    return (
+      <div className="App">
+        <p className="App__message">
+          {this.state.keyPressed !== '' ? (
+            <>The last pressed key is [{this.state.keyPressed}]</>
+          ) : (
+            <>Nothing was pressed yet</>
+          )}
+        </p>
+      </div>
+    );
+  }
+}
