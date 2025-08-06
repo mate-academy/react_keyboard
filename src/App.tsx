861cbd052d6ef7a09f85a6d061add589ef771401
Arber <1234arberzylyftari1234@gmail.com>
2025-08-06 13:55:20 +0200

    Keyboard Task Solution

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,27 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 
-export const App: React.FC = () => (
-  <div className="App">
-    <p className="App__message">The last pressed key is [Enter]</p>
-  </div>
-);
+export const App: React.FC = () => {
+  const [key, setKey] = useState('');
+
+  useEffect(() => {
+    const handleKeyDown = (event: KeyboardEvent): void => {
+      setKey(event.key);
+    };
+
+    window.addEventListener('keydown', handleKeyDown);
+
+    return () => {
+      window.removeEventListener('keydown', handleKeyDown);
+    };
+  }, []);
+
+  return (
+    <div className="App">
+      {key ? (
+        <p className="App__message">The last pressed key is [{key}]</p>
+      ) : (
+        <p className="App__message">Nothing was pressed yet</p>
+      )}
+    </div>
+  );
+};
