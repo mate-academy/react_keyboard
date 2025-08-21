c319e39e43cfaf3796dc0c2eaae43a8aa643c37d
michelam-pseudo <124099775+michelam-pseudo@users.noreply.github.com>
2025-08-21 11:21:22 -0300

    Refactoring with the broker AI specifications.

--- src/App.tsx
+++ src/App.tsx
@@ -1,49 +1,48 @@
-import React, { useRef } from 'react';
+import React from 'react';
+
+type Props = {
+  
+}
 
 type State = {
   pressedKey: string;
-}
+};
 
-export class App extends React.Component<State> {
-  
+export class App extends React.Component<Props, State> {
+  state = {
+    pressedKey: null as string,
+  };
 
-  constructor(props) {
-    super(props);
-    this.state = {
-      pressedKey: ''
-    };
+  handleKeyUp = (event: KeyboardEvent) => {
+    // eslint-disable-next-line no-console
+    this.setState({
+      pressedKey: event.key,
+    })
+  };
 
-    this.handleKeyUp = (event: KeyboardEvent) => {
-      console.log(event.key);
-    }
-
-    this.handleKeyDown = (event: KeyboardEvent) => {
-      this.setState({
-        pressedKey: event.key
-      })
-    };
-  }
+  handleKeyDown = (event: KeyboardEvent) => {
+    this.setState({
+      pressedKey: event.key,
+    });
+  };
 
   componentDidMount() {
-    document.addEventListener('keyup', this.handleKeyUp);
-    window.addEventListener('keypress', this.handleKeyDown);
+    window.addEventListener('keyup', this.handleKeyUp);
   }
 
   componentWillUnmount() {
-    document.removeEventListener('keyup', this.handleKeyUp);
-    document.removeEventListener('keydown', this.handleKeyDown);
+    window.removeEventListener('keyup', this.handleKeyUp);
   }
-  
-  render() {
 
-    return(
+  render() {
+    return (
       <>
-        <div className="App"
-          onKeyUp={this.handleKeyDown}
-        >
-              <p className="App__message">{this.state.pressedKey !== '' ? `The last pressed key is [${this.state.pressedKey}]` : 'Nothing was pressed yet'}</p>
+        <div className="App" onKeyUp={this.handleKeyDown}>
+          <p className="App__message">
+            {this.state.pressedKey !== null ? `The last pressed key is [${this.state.pressedKey}]` : 'Nothing was pressed yet'}
+          </p>
         </div>
       </>
-    )
+    );
   }
 }
