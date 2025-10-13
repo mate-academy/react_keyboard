c7b6608304f36a6cdf935858ca13ff46b23a8ebe
S-Khraban <s.hraban@i.ua>
2025-10-13 15:57:52 +0300

    add solution for keyboard_

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,47 @@
-import React from 'react';
+import React, { Component } from 'react';
+import classNames from 'classnames';
 
-export const App: React.FC = () => (
-  <div className="App">
-    <p className="App__message">The last pressed key is [Enter]</p>
-  </div>
-);
+type AppState = {
+  latestPressedKey: string | null;
+};
+
+export class App extends Component<{}, Readonly<AppState>> {
+  state: Readonly<AppState> = {
+    latestPressedKey: null,
+  };
+
+  private readonly handleDocumentKeyUp = (keyboardEvent: KeyboardEvent) => {
+    this.setState({ latestPressedKey: keyboardEvent.key });
+  };
+
+  componentDidMount(): void {
+    document.addEventListener('keyup', this.handleDocumentKeyUp);
+  }
+
+  componentWillUnmount(): void {
+    document.removeEventListener('keyup', this.handleDocumentKeyUp);
+  }
+
+  render() {
+    const { latestPressedKey } = this.state;
+
+    const applicationContainerClassName = classNames('App');
+
+    const messageClassName = classNames('App__message', {
+      'App__message--empty': latestPressedKey === null,
+      'App__message--withKey': latestPressedKey !== null,
+    });
+
+    return (
+      <div className={applicationContainerClassName}>
+        {latestPressedKey === null ? (
+          <p className={messageClassName}>Nothing was pressed yet</p>
+        ) : (
+          <p className={messageClassName}>
+            The last pressed key is [{latestPressedKey}]
+          </p>
+        )}
+      </div>
+    );
+  }
+}
