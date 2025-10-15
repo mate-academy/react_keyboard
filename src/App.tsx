3459314e664b4257e8c7ca34d29bf1b833d400d6
viktoriyaRi <vikamalik30@gmail.com>
2025-10-15 14:38:11 -0400

    Implement keyboard: class component with key listener + messages

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,40 @@
 import React from 'react';
+import './index.scss';
 
-export const App: React.FC = () => (
-  <div className="App">
-    <p className="App__message">The last pressed key is [Enter]</p>
-  </div>
-);
+type State = {
+  pressedKey: string | null;
+};
+
+export class App extends React.Component<Record<string, never>, State> {
+  state: State = { pressedKey: null };
+
+  private handleKeyUp = (event: KeyboardEvent) => {
+    this.setState({ pressedKey: event.key });
+  };
+
+  componentDidMount() {
+    document.addEventListener('keyup', this.handleKeyUp);
+  }
+
+  componentWillUnmount() {
+    document.removeEventListener('keyup', this.handleKeyUp);
+  }
+
+  render() {
+    const { pressedKey } = this.state;
+
+    return (
+      <main className="App">
+        <p className="App__message">
+          {pressedKey
+            ? <>The last pressed key is [{pressedKey}]</>
+            : 'Nothing was pressed yet'}
+        </p>
+      </main>
+    );
+  }
+}
+
+export default App;
+
+
