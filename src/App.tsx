e7ac8a20ab97e25bc9ed7301590bbfcf8ba5acdf
Danylo <lliivadin@gmail.com>
2025-10-21 11:18:30 +0300

    feat: Add class App component

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,37 @@
 import React from 'react';
 
-export const App: React.FC = () => (
-  <div className="App">
-    <p className="App__message">The last pressed key is [Enter]</p>
-  </div>
-);
+interface AppState {
+  pressKey: string | null;
+}
+
+export class App extends React.Component<{}, AppState> {
+  state: AppState = {
+    pressKey: null,
+  };
+
+  handleKeyUp = (event: KeyboardEvent) => {
+    this.setState({ pressKey: event.key });
+  };
+
+  componentDidMount(): void {
+    document.addEventListener('keyup', this.handleKeyUp);
+  }
+
+  componentWillUnmount(): void {
+    document.removeEventListener('keyup', this.handleKeyUp);
+  }
+
+  render(): React.ReactNode {
+    const { pressKey } = this.state;
+
+    return (
+      <div className="App">
+        <p className="App__message">
+          {pressKey
+            ? `The last pressed key is [${pressKey}]`
+            : 'Nothing was pressed yet'}
+        </p>
+      </div>
+    );
+  }
+}
