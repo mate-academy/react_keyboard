2936f286cbec5fb0657fb1faa39aa226b1056482
Bauzi <volodiasyd@gmail.com>
2025-10-22 21:21:03 +0300

    fix: renamed lastPressedKey to pressedKey to meet the core requirement

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,16 @@
 import React from 'react';
 
 type State = {
-  lastPressedKey: string | null;
+  pressedKey: string | null;
 };
 
 export class App extends React.Component<{}, State> {
   state: State = {
-    lastPressedKey: null,
+    pressedKey: null,
   };
 
   handleKeyboardButtonClick = (event: KeyboardEvent) => {
-    this.setState({ lastPressedKey: event.key });
+    this.setState({ pressedKey: event.key });
   };
 
   componentDidMount(): void {
@@ -22,13 +22,13 @@ export class App extends React.Component<{}, State> {
   }
 
   render() {
-    const { lastPressedKey } = this.state;
+    const { pressedKey } = this.state;
 
     return (
       <div className="App">
         <p className="App__message">
-          {lastPressedKey
-            ? `The last pressed key is [${lastPressedKey}]`
+          {pressedKey
+            ? `The last pressed key is [${pressedKey}]`
             : 'Nothing was pressed yet'}
         </p>
       </div>
