0b37d8be69cd567859ce05ebcd13575a223d9836
Oleksiy Rozsokha <xanderiooo21@gmail.com>
2025-10-23 20:57:50 +0300

    update pressesKey condition structure

--- src/App.tsx
+++ src/App.tsx
@@ -26,12 +26,19 @@ export class App extends React.Component<{}, State> {
     const { pressedKey } = this.state;
 
     return (
+      // <div className="App">
+      //   {pressedKey ? (
+      //     <p className="App__message">{`The last pressed key is [${pressedKey}]`}</p>
+      //   ) : (
+      //     <p className="App__message">Nothing was pressed yet</p>
+      //   )}
+      // </div>
       <div className="App">
-        {pressedKey ? (
-          <p className="App__message">{`The last pressed key is [${pressedKey}]`}</p>
-        ) : (
-          <p className="App__message">Nothing was pressed yet</p>
-        )}
+        <p className="App__message">
+          {pressedKey
+            ? `The last pressed key is [${pressedKey}]`
+            : 'Nothing was pressed yet'}
+        </p>
       </div>
     );
   }
