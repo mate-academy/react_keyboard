b733457331232cf37eb11d0f95ed2fad1cc65a85
DropPredator <ionchiccc@gmail.com>
2025-10-24 11:00:36 +0300

    approve it

--- src/App.tsx
+++ src/App.tsx
@@ -32,7 +32,9 @@ export class App extends React.Component<Props, State> {
   render(): React.ReactNode {
     return (
       <div className="App">
-        <p className="App__message">{`The last pressed key is [${this.state.pressedKey}]`}</p>
+        <p className="App__message">{this.state.pressedKey ? (
+          `The last pressed key is [${this.state.pressedKey}]`
+          ) : 'Nothing was pressed yet'}</p>
       </div>
     );
   }
