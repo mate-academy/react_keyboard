7764aab37feb71526a5a49d41896fddf9b9cc7d9
Afengi <gorodny.ivan4@gmail.com>
2025-11-10 19:12:20 +0200

    solution

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable prettier/prettier */
 import React from 'react';
 
 interface State {
@@ -26,11 +27,16 @@ export class App extends React.Component<{}, State> {
 
     return (
       <div className="App">
-        {pressedKey ? (
-          <p className="App__message">The last pressed key is [{pressedKey}]</p>
-        ) : (
-          <p className="App__message">Nothing was pressed yet</p>
-        )}
+        {pressedKey
+          ? (
+            <p className="App__message">
+              The last pressed key is [{pressedKey}]
+            </p>
+          ) : (
+            <p className="App__message">
+              Nothing was pressed yet
+            </p>
+          )}
       </div>
     );
   }
