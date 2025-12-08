bfb1eeb840d6ee934d5466720f1220e14bcbdefb
Serhii Mamchur <kovaniysvit@gmail.com>
2025-12-08 10:41:15 +0100

    Solution2

--- src/App.tsx
+++ src/App.tsx
@@ -10,15 +10,16 @@ export class App extends React.Component<{}, State> {
   };
 
   componentDidMount(): void {
-    document.addEventListener('keyup', (event: KeyboardEvent) => {
-      this.setState({ pressedKey: event.key });
-    });
+    document.addEventListener(
+      'keyup',
+      (this.handlePressedKeychange = (event: KeyboardEvent) => {
+        this.setState({ pressedKey: event.key });
+      }),
+    );
   }
 
   componentWillUnmount(): void {
-    document.removeEventListener('keyup', (event: KeyboardEvent) => {
-      this.setState({ pressedKey: event.key });
-    });
+    document.removeEventListener('keyup', this.handlePressedKeychange());
   }
 
   // handlePressedKeychange = (event: KeyboardEvent) => {
