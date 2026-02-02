ab166f92931f3ff2a658b2830fcb04cd12252883
Anastasiia Levochkina <nastya.mixaylova98@gmail.com>
2026-02-02 04:01:51 +0200

    add task solution

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,7 @@
-import { createRoot } from 'react-dom/client';
+import ReactDOM from 'react-dom/client';
 import { App } from './App';
-
 import './index.scss';
 
-createRoot(document.getElementById('root') as HTMLElement).render(<App />);
+ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
+  <App />,
+);
