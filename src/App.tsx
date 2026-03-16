054b8502bf846f6e1a928ef342f06b20749bd244
Ihor Haidai <igayday74@gmail.com>
2026-03-16 16:10:17 -0400

    Final solution

--- src/App.tsx
+++ src/App.tsx
@@ -28,11 +28,19 @@ export class App extends React.Component<{}, State> {
 
     return (
       <div className="App">
-        {!pressedKey ? (
-          <p className="App__message">Nothing was pressed yet</p>
-        ) : (
-          <p className="App__message">The last pressed key is [{pressedKey}]</p>
-        )}
+        {
+          !pressedKey
+            ? (
+              <p className="App__message">
+                Nothing was pressed yet
+              </p>
+            )
+            : (
+              <p className="App__message">
+                The last pressed key is [{pressedKey}]
+              </p>
+            )
+        }
       </div>
     );
   }
