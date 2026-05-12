1b361bb296ff6dbb7d3414ef4a6208050c4727b2
Roman Lysunets <rlysunets@gmail.com>
2026-05-12 17:19:14 +0300

    add task solution fix

--- src/App.tsx
+++ src/App.tsx
@@ -1,31 +1,76 @@
-import React, { useState, useEffect } from 'react';
+// import React, { useState, useEffect } from 'react';
 
-export const App: React.FC = () => {
-  const [lastKey, setLastKey] = useState<string>('');
+// export const App: React.FC = () => {
+//   const [lastKey, setLastKey] = useState<string>('');
 
-  useEffect(() => {
-    const handleKeyDown = (event: KeyboardEvent) => {
-      setLastKey(event.key);
-    };
+//   useEffect(() => {
+//     const handleKeyDown = (event: KeyboardEvent) => {
+//       setLastKey(event.key);
+//     };
 
-    window.addEventListener('keyup', handleKeyDown);
+//     window.addEventListener('keyup', handleKeyDown);
 
-    return () => {
-      window.removeEventListener('keyup', handleKeyDown);
-    };
-  }, []);
+//     return () => {
+//       window.removeEventListener('keyup', handleKeyDown);
+//     };
+//   }, []);
 
-  if (lastKey.length === 0) {
+//   if (lastKey.length === 0) {
+//     return (
+//       <div className="App">
+//         <p className="App__message">Nothing was pressed yet</p>
+//       </div>
+//     );
+//   }
+
+//   return (
+//     <div className="App">
+//       <p className="App__message">The last pressed key is [{lastKey}]</p>
+//     </div>
+//   );
+// };
+
+
+import React from 'react';
+
+type State = {
+  lastKey: string;
+};
+
+export class App extends React.Component<{}, State> {
+  state: State = {
+    lastKey: '',
+  };
+
+  handleKeyUp = (event: KeyboardEvent) => {
+    this.setState({ lastKey: event.key });
+  };
+
+  componentDidMount() {
+    window.addEventListener('keyup', this.handleKeyUp);
+  }
+
+  componentWillUnmount() {
+    window.removeEventListener('keyup', this.handleKeyUp);
+  }
+
+  render() {
+    const { lastKey } = this.state;
+
+    if (lastKey.length === 0) {
+      return (
+        <div className="App">
+          <p className="App__message">Nothing was pressed yet</p>
+        </div>
+      );
+    }
+
     return (
       <div className="App">
-        <p className="App__message">Nothing was pressed yet</p>
+        <p className="App__message">
+          The last pressed key is [{lastKey}]
+        </p>
       </div>
     );
   }
-
-  return (
-    <div className="App">
-      <p className="App__message">The last pressed key is [{lastKey}]</p>
-    </div>
-  );
-};
+}
