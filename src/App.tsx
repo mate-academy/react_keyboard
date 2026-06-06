36eda481cfa16ec1a6e32ed98c7ea73363c73b0a
Taras Pidkuimukha <taraspidkuimukha@gmail.com>
2026-06-06 15:32:55 +0200

    feat: class component is adde

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,28 @@
 import React from 'react';
 
-export const App: React.FC = () => (
-  <div className="App">
-    <p className="App__message">The last pressed key is [Enter]</p>
-  </div>
-);
+export class App extends React.Component {
+  state = {
+    pressedKey: null,
+  }
+
+  handleKeyUp = (event: KeyboardEvent) => {
+    this.setState({ pressedKey: event.key })
+  }
+  componentDidMount(): void {
+    document.addEventListener('keyup', this.handleKeyUp);
+  }
+  componentWillUnmount(): void {
+    document.removeEventListener('keyup', this.handleKeyUp);
+  }
+
+  render() {
+    return (
+      <div className='App__message'>
+        {this.state.pressedKey
+          ? <p>The last pressed key is [{this.state.pressedKey}]</p>
+          : <p>Nothing was pressed yet</p>
+        }
+      </div>
+    )
+  }
+}
