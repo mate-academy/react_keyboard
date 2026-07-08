24577e3f5cebc4b44151a57b7e1f2b79f825abd9
Yurii-Leniv <supusman32@gmail.com>
2026-07-08 13:10:14 +0300

    lwepfkjeihgkqgfqw

--- src/App.tsx
+++ src/App.tsx
@@ -26,11 +26,11 @@ export class App extends React.Component<{}, State> {
 
     return (
       <div className="App">
-        {pressedKey === null ? (
-          <p className="App__message">Nothing was pressed yet</p>
-        ) : (
-          <p className="App__message">The last pressed key is {pressedKey}</p>
-        )}
+        <p className="App__message">
+          {pressedKey === null
+            ? 'Nothing was pressed yet'
+            : `The last pressed key is [${pressedKey}]`}
+        </p>
       </div>
     );
   }
