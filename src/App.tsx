5c81f152ebe36cca7decd5ac01f644efade6a032
Anna Skobara <annaskobara14@gmail.com>
2026-07-22 14:22:09 +0300

    Task

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,35 @@
-import React from 'react';
+import React, { Component } from 'react';
 
-export const App: React.FC = () => (
-  <div className="App">
-    <p className="App__message">The last pressed key is [Enter]</p>
-  </div>
-);
+interface State {
+  pressedKey: string | null;
+}
+
+export class App extends Component<{}, State> {
+  state: State = {
+    pressedKey: null,
+  };
+
+  handleKeyUp = (event: KeyboardEvent) => {
+    this.setState({ pressedKey: event.key });
+  };
+
+  componentDidMount() {
+    document.addEventListener('keyup', this.handleKeyUp);
+  }
+
+  componentWillUnmount() {
+    document.removeEventListener('keyup', this.handleKeyUp);
+  }
+
+  render() {
+    const { pressedKey } = this.state;
+
+    return (
+      <div>
+        {pressedKey === null
+          ? 'Nothing was pressed yet'
+          : `The last pressed key is [${pressedKey}]`}
+      </div>
+    );
+  }
+}
