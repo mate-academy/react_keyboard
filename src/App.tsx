07c52d5c5e95e5c0872760d95f6fe40941acf97c
student1232354 <tarasromanenko33@gmail.com>
2026-07-26 16:10:43 +0300

    first update

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,37 @@
-import React from 'react';
+import React, { Component } from 'react';
 
-export const App: React.FC = () => (
-  <div className="App">
-    <p className="App__message">The last pressed key is [Enter]</p>
-  </div>
-);
+interface LikeNew {
+  PressedKey: string | null;
+}
+
+export class App extends Component<{}, LikeNew> {
+  state: LikeNew = {
+    PressedKey: null,
+  };
+
+  thiskey = (event: KeyboardEvent) => {
+    this.setState({ PressedKey: event.key });
+  };
+
+  componentDidMount(): void {
+    document.addEventListener('keyup', this.thiskey);
+  }
+
+  componentWillUnmount(): void {
+    document.removeEventListener('keyup', this.thiskey);
+  }
+
+  render() {
+    const { PressedKey } = this.state;
+
+    return (
+      <div className="App">
+        <p className="App__message">
+          {PressedKey !== null
+            ? `The last pressed key is ${PressedKey}`
+            : `haven't pushed keys`}
+        </p>
+      </div>
+    );
+  }
+}
