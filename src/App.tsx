c0430886c73910a9ec433e26dd9b4d06dbc386a0
Roman <romanpopovuch07@gmail.com>
2026-08-10 20:42:38 +0300

    add task solution

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,40 @@
 import React from 'react';
 
-export const App: React.FC = () => (
-  <div className="App">
-    <p className="App__message">The last pressed key is [Enter]</p>
-  </div>
-);
+interface State {
+  pressedKey: string | null;
+}
+
+export class App extends React.Component<{}, State> {
+  state: State = {
+    pressedKey: null,
+  };
+
+  componentDidMount() {
+    // Adding the global event listener when the component mounts
+    document.addEventListener('keyup', this.handleKeyUp);
+  }
+
+  componentWillUnmount() {
+    // Cleaning up the global event listener before the component unmounts
+    document.removeEventListener('keyup', this.handleKeyUp);
+  }
+
+  // Using an arrow function automatically binds `this`
+  handleKeyUp = (event: KeyboardEvent) => {
+    this.setState({ pressedKey: event.key });
+  };
+
+  render() {
+    const { pressedKey } = this.state;
+
+    return (
+      <div className="App">
+        <p className="App__message">
+          {pressedKey === null
+            ? 'Nothing was pressed yet'
+            : `The last pressed key is [${pressedKey}]`}
+        </p>
+      </div>
+    );
+  }
+}
