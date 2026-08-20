86c2e4393a5b4d7a49fd8ed7bd3667e9933bd4f9
Ваня <bigmag@MacBook-Air.local>
2026-08-20 18:17:02 +0300

    add removeEventListener

--- src/App.tsx
+++ src/App.tsx
@@ -2,25 +2,35 @@ import React from 'react';
 
 type State = {
   pressedKey: string;
-}
+};
 export class App extends React.Component<State> {
   state = {
     pressedKey: '',
-  }
+  };
+
+  private handleKeyApp = (event: KeyboardEvent): void => {
+    this.setState({ pressedKey: event.key });
+  };
 
   componentDidMount(): void {
-    document.addEventListener('keyup', (event: KeyboardEvent) => {
-      this.setState({pressedKey: event.key});
-    });
+    document.addEventListener('keyup', this.handleKeyApp);
+  }
+
+  componentWillUnmount(): void {
+    document.removeEventListener('keyup', this.handleKeyApp);
   }
 
   render() {
-    const { pressedKey } = this.state
+    const { pressedKey } = this.state;
 
     return (
       <div className="App">
-        {pressedKey ? <p className="App__message">The last pressed key is [{pressedKey}]</p> : <p className="App__message">Nothing was pressed yet</p>}
+        {pressedKey ? (
+          <p className="App__message">The last pressed key is [{pressedKey}]</p>
+        ) : (
+          <p className="App__message">Nothing was pressed yet</p>
+        )}
       </div>
-    )
+    );
   }
 }
