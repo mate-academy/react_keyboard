1b80e01df61fb05f23fa8e16014610900486acf4
sofiatsebenko <sofialtd50@gmail.com>
2026-09-14 19:14:09 +0300

    done

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,13 @@
-import React from "react";
-
+import React from 'react';
 
 type State = {
   pressedKey: string | null;
-}
+};
 
-
-export class App extends React.Component <{}, State> {
+export class App extends React.Component<{}, State> {
   state: State = {
     pressedKey: null,
-  }
+  };
 
   handleKeyUp = (event: KeyboardEvent) => {
     this.setState({ pressedKey: event.key });
@@ -24,12 +22,16 @@ export class App extends React.Component <{}, State> {
   }
 
   render() {
-    const {pressedKey} = this.state;
+    const { pressedKey } = this.state;
+
     return (
       <div className="App">
-        <p className="App__message">{pressedKey ? `The last pressed key is ${pressedKey}` : `Nothing was pressed yet`}</p>
+        <p className="App__message">
+          {pressedKey
+            ? `The last pressed key is [${pressedKey}]`
+            : `Nothing was pressed yet`}
+        </p>
       </div>
-    )
-
+    );
   }
-};
+}
