a93fb49dbd40d5a39896f7d76dd0ea4907ffdefb
Santana <wonderday1804@gmail.com>
2026-09-15 18:55:09 +0300

    fix problems

--- src/App.tsx
+++ src/App.tsx
@@ -6,6 +6,9 @@ export class App extends React.Component {
   };
 
   handleKeyUp = (event: KeyboardEvent) => {
+    // eslint-disable-next-line no-console
+    console.log(event.key);
+
     this.setState({
       pressedKey: event.key,
     });
